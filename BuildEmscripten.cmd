392c39c6ceb48d09fc5e6f0f64c632c78f7087a6
ducph <ducph5@vng.com.vn>
2019-11-26 11:34:16 +0700

    feat: #3 Fix wrong env for CI Android

--- BuildEmscripten.cmd
+++ BuildEmscripten.cmd
@@ -1,3 +1,3 @@
 call C:\emsdk\emsdk_env.bat
-cmake -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\fastcomp\emscripten\cmake\Modules\Platform\Emscripten.cmake  -S . -B ./PrjEmscripten -G "MinGW Makefiles" .
+cmake -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\fastcomp\emscripten\cmake\Modules\Platform\Emscripten.cmake  -S . -B ./PrjEmscripten -G "MinGW Makefiles"
 mingw32-make -C PrjEmscripten
