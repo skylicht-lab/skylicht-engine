98fe9af5ea4107a0e184d9ba3af923c8f1ccff82
ducphamhong <hongduc.pr@gmail.com>
2020-01-12 12:45:34 +0700

    feat: #47 Add export material

--- Projects/Skylicht/Engine/Source/MeshManager/CMeshManager.h
+++ Projects/Skylicht/Engine/Source/MeshManager/CMeshManager.h
@@ -18,6 +18,8 @@ namespace Skylicht
 
 		CEntityPrefab* loadModel(const char *resource, const char *texturePath, bool loadNormalMap = true, bool loadTexcoord2 = false, bool createBatching = false);
 
+		void exportMaterial(CEntityPrefab *prefab, const char *folder, const char *filename);
+
 		void releaseAllPrefabs();
 	};
 }
