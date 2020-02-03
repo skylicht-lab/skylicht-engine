750af049d6bd3410e6f8be277031736d805df7fd
ducphamhong <hongduc.pr@gmail.com>
2020-02-03 18:58:23 +0700

    feat: #48 Add spec glos mask shader

---
+++ Assets/BuiltIn/Shader/SpecularGlossiness/Deferred/GLSL/SpecularGlossinessMaskFS.glsl
@@ -0,0 +1,42 @@
+precision highp float;
+
+uniform sampler2D uTexDiffuse;
+uniform sampler2D uTexNormal;
+uniform sampler2D uTexSpecGloss;
+uniform sampler2D uTexMask;
+
+uniform float uCutoff;
+
+in vec2 vTexCoord0;
+in vec3 vWorldPosition;
+in vec3 vWorldNormal;
+in vec3 vWorldTangent;
+in vec3 vWorldBinormal;
+in float vTangentW;
+
+layout (location = 0) out vec4 Diffuse;
+layout (location = 1) out vec4 Position;
+layout (location = 2) out vec4 Normal;
+layout (location = 3) out vec4 SG;
+
+void main(void)
+{
+	vec3 maskMap = texture(uTexMask, vTexCoord0.xy).xyz;
+	if (maskMap.r < uCutoff)
+		discard;
+	
+	vec3 baseMap = texture(uTexDiffuse, vTexCoord0.xy).xyz;
+	vec3 normalMap = texture(uTexNormal, vTexCoord0.xy).xyz;
+	vec3 sgMap = texture(uTexSpecGloss, vTexCoord0.xy).xyz;
+	
+	mat3 rotation = mat3(vWorldTangent, vWorldBinormal, vWorldNormal);
+	vec3 localCoords = normalMap * 2.0 - vec3(1.0);
+	localCoords.y = localCoords.y * vTangentW;
+	vec3 n = rotation * localCoords;
+	n = normalize(n);
+	
+	Diffuse = vec4(baseMap, 1.0);
+	Position = vec4(vWorldPosition, 1.0);
+	Normal = vec4(n, 1.0);
+	SG = vec4(sgMap, 1.0);
+}
---
+++ Assets/BuiltIn/Shader/SpecularGlossiness/Deferred/HLSL/SpecularGlossinessMaskFS.hlsl
@@ -0,0 +1,62 @@
+Texture2D uTexDiffuse : register(t0);
+SamplerState uTexDiffuseSampler : register(s0);
+
+Texture2D uTexNormal : register(t1);
+SamplerState uTexNormalSampler : register(s1);
+
+Texture2D uTexSpecGloss : register(t2);
+SamplerState uTexSpecGlossSampler : register(s2);
+
+Texture2D uTexMask : register(t3);
+SamplerState uTexMaskSampler : register(s3);
+
+struct PS_INPUT
+{
+	float4 pos : SV_POSITION;
+	float2 tex0 : TEXCOORD0;
+	float3 worldPosition: WORLDPOSITION;
+	float3 worldNormal: WORLDNORMAL;
+	float3 worldTangent: WORLDTANGENT;
+	float3 worldBinormal: WORLDBINORMAL;
+	float tangentw : TANGENTW;
+};
+
+struct PS_OUTPUT
+{
+	float4 Diffuse: SV_TARGET0;
+	float4 Position: SV_TARGET1;
+	float4 Normal: SV_TARGET2;
+	float4 SG: SV_TARGET3;
+};
+
+cbuffer cbPerFrame
+{
+	float uCutoff;
+};
+
+PS_OUTPUT main(PS_INPUT input)
+{
+	PS_OUTPUT output;
+
+	float3 maskMap = uTexMask.Sample(uTexMaskSampler, input.tex0).xyz;
+	if (maskMap.r < uCutoff)
+		discard;
+
+	float3 baseMap = uTexDiffuse.Sample(uTexDiffuseSampler, input.tex0).xyz;
+	float3 normalMap = uTexNormal.Sample(uTexNormalSampler, input.tex0).xyz;
+	float3 sgMap = uTexSpecGloss.Sample(uTexSpecGlossSampler, input.tex0).xyz;
+
+	float3x3 rotation = float3x3(input.worldTangent, input.worldBinormal, input.worldNormal);
+
+	float3 localCoords = normalMap * 2.0 - float3(1.0, 1.0, 1.0);
+	localCoords.y *= input.tangentw;
+	float3 n = mul(localCoords, rotation);
+	n = normalize(n);
+
+	output.Diffuse = float4(baseMap, 1.0);
+	output.Position = float4(input.worldPosition, 1.0);
+	output.Normal = float4(n, 1.0);
+	output.SG = float4(sgMap, 1.0);
+	
+	return output;
+}
