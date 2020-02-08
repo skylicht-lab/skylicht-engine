9a740224e52614af50cc04404189972436dfac2e
ducphamhong <hongduc.pr@gmail.com>
2020-02-08 18:06:41 +0700

    Update build etc/pvr Android

--- Projects/Skylicht/Engine/Source/TextureManager/CTextureManager.cpp
+++ Projects/Skylicht/Engine/Source/TextureManager/CTextureManager.cpp
@@ -153,13 +153,16 @@ namespace Skylicht
 		// try to load compress texture
 		if (driver->getDriverType() == video::EDT_OPENGLES)
 		{
-#if defined(IOS)
-			CStringImp::replacePathExt(ansiPath, ".pvr");
+#if defined(ANDROID)
+			// etc compress
+			CStringImp::replacePathExt(ansiPath, ".epvr");
 #elif defined(__EMSCRIPTEN__)
 			if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_DXT) == true)
 				CStringImp::replacePathExt(ansiPath, ".dds");
-			else
+			else if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC) || driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC2))
 				CStringImp::replacePathExt(ansiPath, ".pvr");
+			else
+				CStringImp::replacePathExt(ansiPath, ".epvr");
 #else
 			CStringImp::replacePathExt(ansiPath, ".pvr");
 #endif
@@ -214,17 +217,20 @@ namespace Skylicht
 		// try to load compress texture
 		if (driver->getDriverType() == video::EDT_OPENGLES)
 		{
-#if defined(IOS)
-			CStringImp::replacePathExt(ansiPath, ".pvr");
+#if defined(ANDROID)
+			// etc compress
+			CStringImp::replacePathExt(ansiPath, ".epvr");
 #elif defined(__EMSCRIPTEN__)
 			if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_DXT) == true)
 				CStringImp::replacePathExt(ansiPath, ".dds");
-			else
+			else if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC) || driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC2))
 				CStringImp::replacePathExt(ansiPath, ".pvr");
+			else
+				CStringImp::replacePathExt(ansiPath, ".epvr");
 #else
 			CStringImp::replacePathExt(ansiPath, ".pvr");
 #endif
-	}
+		}
 		else if (driver->getDriverType() == video::EDT_DIRECT3D11 || driver->getDriverType() == video::EDT_OPENGL)
 		{
 			CStringImp::replacePathExt(ansiPath, ".dds");
@@ -283,7 +289,7 @@ namespace Skylicht
 			m_nullTexture = driver->getTexture("BuiltIn/Textures/NullTexture.png");
 
 		return texture;
-}
+	}
 
 	ITexture* CTextureManager::getTextureArray(std::vector<std::string>& listTexture)
 	{
@@ -311,16 +317,21 @@ namespace Skylicht
 
 			// try to load compress texture
 			if (driver->getDriverType() == video::EDT_OPENGLES)
-#if defined(IOS)
-				CStringImp::replacePathExt(ansiPath, ".pvr");
+			{
+#if defined(ANDROID)
+				// etc compress
+				CStringImp::replacePathExt(ansiPath, ".epvr");
 #elif defined(__EMSCRIPTEN__)
 				if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_DXT) == true)
 					CStringImp::replacePathExt(ansiPath, ".dds");
-				else
+				else if (driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC) || driver->queryFeature(EVDF_TEXTURE_COMPRESSED_PVRTC2))
 					CStringImp::replacePathExt(ansiPath, ".pvr");
+				else
+					CStringImp::replacePathExt(ansiPath, ".epvr");
 #else
 				CStringImp::replacePathExt(ansiPath, ".pvr");
 #endif
+			}
 			else if (driver->getDriverType() == video::EDT_DIRECT3D11 || driver->getDriverType() == video::EDT_OPENGL)
 				CStringImp::replacePathExt(ansiPath, ".dds");
 
