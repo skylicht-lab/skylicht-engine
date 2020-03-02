cdc1cd87abf7eee9b86ffebf1db1fb1abf48fb4b
ducph <ducph5@vng.com.vn>
2020-03-02 12:50:43 +0700

    feat: #50 Implement GUIText

--- Projects/Skylicht/Engine/Source/Graphics2D/GUI/CGUIText.h
+++ Projects/Skylicht/Engine/Source/Graphics2D/GUI/CGUIText.h
@@ -25,22 +25,123 @@ https://github.com/skylicht-lab/skylicht-engine
 #pragma once
 
 #include "CGUIElement.h"
+#include "Graphics2D/SpriteFrame/IFont.h"
+
+#define MAX_FORMATCOLOR 32
 
 namespace Skylicht
 {
 	class CGUIText : public CGUIElement
 	{
 		friend class CCanvas;
+	public:
+		typedef std::vector<int> ArrayInt;
+		typedef std::vector<SModuleOffset*> ArrayModuleOffset;
+
 	protected:
+		IFont *m_sprite;
+
+		int m_charPadding;
+		int m_charSpacePadding;
+		int m_linePadding;
+
+		std::string m_text;
+		std::wstring m_textw;
+		ArrayInt m_textFormat;
+
+		EGUIVerticalAlign TextVertical;
+		EGUIHorizontalAlign TextHorizontal;
+
+		int m_textHeight;
+		int m_textSpaceWidth;
+
+		bool m_multiLine;
+		bool m_centerRotate;
+
+		SColor m_colorFormat[MAX_FORMATCOLOR];
+
+		bool m_updateTextRender;
+
+		std::vector<ArrayModuleOffset> m_arrayCharRender;
+		std::vector<ArrayInt> m_arrayCharFormat;
 
 	protected:
 		CGUIText(CCanvas *canvas, const core::rectf& rect);
 		CGUIText(CCanvas *canvas, CGUIElement *parent);
 		CGUIText(CCanvas *canvas, CGUIElement *parent, const core::rectf& rect);
 
+		virtual void renderText(ArrayModuleOffset &string, ArrayInt& format, int posY);
+
+		void init();
+
 	public:
 		virtual ~CGUIText();
 
 		virtual void render(CCamera *camera);
+
+		void splitText(std::vector<ArrayModuleOffset>& split, std::vector<ArrayInt>& format, int width);
+
+		void setTextAlign(EGUIHorizontalAlign h, EGUIVerticalAlign v)
+		{
+			TextVertical = v;
+			TextHorizontal = h;
+		}
+
+		/*
+		* Example:
+		* <1>PLAYERA <0>kill <1>PLAYERB <0>with <2>AK47
+		* With format:
+		* <1> -> RED
+		* <0> -> WHITE
+		* <2> -> YELLOW
+		*/
+		void setFormatText(const char *formatText);
+
+		// setColorFormat
+		// set color for function setFormatText
+		void setColorFormat(int id, const SColor& c)
+		{
+			if (id < MAX_FORMATCOLOR)
+				m_colorFormat[id] = c;
+		}
+
+		void setText(const char *text);
+		void setTextStrim(const char *text);
+		void setText(const wchar_t *text);
+		void setTextStrim(const wchar_t *text);
+
+		int getStringWidth(const char *text);
+		int getCharWidth(wchar_t c);
+
+		const char* getText()
+		{
+			return m_text.c_str();
+		}
+
+		const wchar_t* getTextW()
+		{
+			return m_textw.c_str();
+		}
+
+		inline void setCharPadding(int charPadding)
+		{
+			m_charPadding = charPadding;
+			m_charSpacePadding = charPadding;
+		}
+
+		inline void setLinePadding(int linePadding)
+		{
+			m_linePadding = linePadding;
+		}
+
+		inline void setMultiLine(bool b)
+		{
+			m_multiLine = b;
+		}
+
+		inline void setCenterRotate(bool b)
+		{
+			m_centerRotate = b;
+		}
 	};
 }
