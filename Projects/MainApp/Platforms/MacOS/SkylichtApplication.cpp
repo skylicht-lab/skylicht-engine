207633fa6ab3aec60d61dcfa8f227cb6adcfa43c
Duc Pham Hong <hongduc.pr@gmail.com>
2020-04-22 14:59:22 +0700

    feat: #62 Add mouse event MacOS

--- Projects/MainApp/Platforms/MacOS/SkylichtApplication.cpp
+++ Projects/MainApp/Platforms/MacOS/SkylichtApplication.cpp
@@ -12,7 +12,12 @@ CApplication *g_mainApp = NULL;
 IrrlichtDevice *g_device = NULL;
 
 SkylichtApplication::SkylichtApplication(int argc, char **argv)
-    :AngleApplication(std::string("Skylicht Engine - DEMO"), argc, argv, 3, 0)
+    :AngleApplication(std::string("Skylicht Engine - DEMO"), argc, argv, 3, 0),
+    m_shiftHold(false),
+    m_controlHold(false),
+    m_leftMouseDown(false),
+    m_midMouseDown(false),
+    m_rightMouseDown(false)
 {
     g_mainApp = new CApplication();
     
@@ -54,12 +59,124 @@ void SkylichtApplication::destroy()
 void SkylichtApplication::step(float dt, double totalTime)
 {
     AngleApplication::step(dt, totalTime);
-    
-    g_device->run();
-    g_mainApp->mainLoop();
 }
 
 void SkylichtApplication::draw()
 {
-    AngleApplication::draw();
+    g_device->run();
+    g_mainApp->mainLoop();
+}
+
+void SkylichtApplication::onKeyUp(const Event::KeyEvent &keyEvent)
+{
+    m_shiftHold = keyEvent.Shift;
+    m_controlHold = keyEvent.Control;
+}
+
+void SkylichtApplication::onKeyDown(const Event::KeyEvent &keyEvent)
+{
+    m_shiftHold = keyEvent.Shift;
+    m_controlHold = keyEvent.Control;
+}
+
+void SkylichtApplication::onMouseMoved(const Event::MouseMoveEvent &mouseEvent)
+{
+    irr::SEvent event;
+    event.EventType = irr::EET_MOUSE_INPUT_EVENT;
+    event.MouseInput.Event = irr::EMIE_MOUSE_MOVED;
+    event.MouseInput.X = mouseEvent.X;
+    event.MouseInput.Y = mouseEvent.Y;
+    event.MouseInput.Shift = m_shiftHold;
+    event.MouseInput.Control = m_controlHold;
+    
+    if (m_leftMouseDown)
+        event.MouseInput.ButtonStates |= irr::EMBSM_LEFT;
+    
+    if (m_midMouseDown)
+        event.MouseInput.ButtonStates |= irr::EMBSM_MIDDLE;
+    
+    if (m_rightMouseDown)
+        event.MouseInput.ButtonStates |= irr::EMBSM_RIGHT;
+    
+    IrrlichtDevice *dev  = getIrrlichtDevice();
+    if (dev)
+        dev->postEventFromUser(event);
+}
+
+void SkylichtApplication::onMouseButtonPressed(const Event::MouseButtonEvent &mouseEvent)
+{
+    bool sendEvent = false;
+    
+    irr::SEvent event;
+    event.EventType = irr::EET_MOUSE_INPUT_EVENT;
+    
+    if (mouseEvent.Button == MOUSEBUTTON_LEFT)
+    {
+        event.MouseInput.Event = irr::EMIE_LMOUSE_PRESSED_DOWN;
+        m_leftMouseDown = true;
+        sendEvent = true;
+    }
+    else if (mouseEvent.Button == MOUSEBUTTON_MIDDLE)
+    {
+        event.MouseInput.Event = irr::EMIE_MMOUSE_PRESSED_DOWN;
+        m_midMouseDown = true;
+        sendEvent = true;
+    }
+    else if (mouseEvent.Button == MOUSEBUTTON_RIGHT)
+    {
+        event.MouseInput.Event = irr::EMIE_RMOUSE_PRESSED_DOWN;
+        m_rightMouseDown = true;
+        sendEvent = true;
+    }
+    
+    event.MouseInput.X = mouseEvent.X;
+    event.MouseInput.Y = mouseEvent.Y;
+    event.MouseInput.Shift = m_shiftHold;
+    event.MouseInput.Control = m_controlHold;
+    
+    if (sendEvent == true)
+    {
+        IrrlichtDevice *dev  = getIrrlichtDevice();
+        if (dev)
+            dev->postEventFromUser(event);
+    }
+}
+
+void SkylichtApplication::onMouseButtonRelease(const Event::MouseButtonEvent &mouseEvent)
+{
+    bool sendEvent = false;
+    
+    irr::SEvent event;
+    event.EventType = irr::EET_MOUSE_INPUT_EVENT;
+    
+    if (mouseEvent.Button == MOUSEBUTTON_LEFT)
+    {
+        event.MouseInput.Event = irr::EMIE_LMOUSE_LEFT_UP;
+        m_leftMouseDown = false;
+        sendEvent = true;
+    }
+    else if (mouseEvent.Button == MOUSEBUTTON_MIDDLE)
+    {
+        event.MouseInput.Event = irr::EMIE_MMOUSE_LEFT_UP;
+        m_midMouseDown = false;
+        sendEvent = true;
+    }
+    else if (mouseEvent.Button == MOUSEBUTTON_RIGHT)
+    {
+        event.MouseInput.Event = irr::EMIE_RMOUSE_LEFT_UP;
+        m_leftMouseDown = false;
+        sendEvent = true;
+    }
+    
+    event.MouseInput.X = mouseEvent.X;
+    event.MouseInput.Y = mouseEvent.Y;
+    event.MouseInput.Shift = m_shiftHold;
+    event.MouseInput.Control = m_controlHold;
+    
+    if (sendEvent == true)
+    {
+        IrrlichtDevice *dev  = getIrrlichtDevice();
+        if (dev)
+            dev->postEventFromUser(event);
+    }
 }
