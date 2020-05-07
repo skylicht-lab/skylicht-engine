dca69dbd6e84f9e3003cafbf3331b6c956592335
ducph <ducph5@vng.com.vn>
2020-05-07 13:41:16 +0700

    feat: #66 Fix build UWP

--- BuildCommand/GenerateUWPrj.cmd
+++ BuildCommand/GenerateUWPrj.cmd
@@ -4,9 +4,9 @@ python BuildAssetBundles.py
 cd ..
 mkdir PrjUWP
 mkdir PrjUWP\Projects
-mkdir PrjUWP\Projects\MainApp
-mkdir PrjUWP\Projects\MainApp\Assets
-copy Projects\MainApp\Platforms\UWP\Assets\*.* PrjUWP\Projects\MainApp\Assets
-copy Bin\*.zip PrjUWP\Projects\MainApp\Assets
+mkdir PrjUWP\Projects\Template
+mkdir PrjUWP\Projects\Template\Assets
+copy Projects\Template\Platforms\UWP\Assets\*.* PrjUWP\Projects\Template\Assets
+copy Bin\*.zip PrjUWP\Projects\Template\Assets
 
 cmake -S . -B ./PrjUWP -G "Visual Studio 15 2017" -A x64 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0
