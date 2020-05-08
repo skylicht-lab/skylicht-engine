338029e249fa06cd5af80b61e83158c8b7b87e8d
ducph <ducph5@vng.com.vn>
2020-05-08 13:31:40 +0700

    feat: #54 Update SH9 compute

--- Projects/Skylicht/Lightmapper/Source/Lightmapper/Baker/CRenderToTexture.cpp
+++ Projects/Skylicht/Lightmapper/Source/Lightmapper/Baker/CRenderToTexture.cpp
@@ -102,20 +102,18 @@ namespace Skylicht
 
 			driver->setRenderTarget(NULL, false, false);
 
-			// Cubemap to SH
-			// https://github.com/nicknikolov/cubemap-sh/blob/master/index.js
-			// https://github.com/nicknikolov/glsl-sh
-
+			// Cubemap to SH		
 			u8 *imageData = (u8*)m_radiance->lock(video::ETLM_READ_ONLY);
 			u32 bpp = 4;
 			u32 rowSize = RT_SIZE * NUM_FACES * bpp;
 			float c = 1.0f / 255.0f;
 
-			for (int i = 0; i < 9; i++)
-				m_sh9[i].set(0.0f, 0.0f, 0.0f);
-
+			core::vector3df sh[9];
 			core::vector3df color;
 			core::vector3df dirTS;
+
+			// Compute the final weight for integration
+			float weightSum = 0.0f;
 
 			// Compute SH by radiance
 			for (u32 face = 0; face < NUM_FACES; face++)
@@ -132,10 +130,11 @@ namespace Skylicht
 					{
 						// Calculate the location in [-1, 1] texture space
 						float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
-						float v = -((y / float(RT_SIZE)) * 2.0f - 1.0f);
+						float v = ((y / float(RT_SIZE)) * 2.0f - 1.0f);
 
 						float temp = 1.0f + u * u + v * v;
 						float weight = 4.0f / (sqrt(temp) * temp);
+						weightSum = weightSum + weight;
 
 						color.X = data[0] * c * weight; // r
 						color.Y = data[1] * c * weight; // g
@@ -147,7 +146,10 @@ namespace Skylicht
 						toTangentSpace[face].rotateVect(dirTS);
 						dirTS.normalize();
 
-						projectOntoSH(dirTS, color, m_sh9);
+						projectOntoSH(dirTS, color, sh);
+
+						for (int i = 0; i < 9; i++)
+							m_sh9[i] = m_sh9[i] + sh[i];
 
 						data += bpp;
 					}
@@ -155,25 +157,6 @@ namespace Skylicht
 					faceData += rowSize;
 				}
 			}
-
-			// Compute the final weight for integration
-			float weightSum = 0.0f;
-			for (u32 y = 0; y < RT_SIZE; y++)
-			{
-				for (u32 x = 0; x < RT_SIZE; x++)
-				{
-					// Calculate the location in [-1, 1] texture space
-					float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
-					float v = ((y / float(RT_SIZE)) * 2.0f - 1.0f);
-
-					float temp = 1.0f + u * u + v * v;
-					float weight = 4.0f / (sqrt(temp) * temp);
-
-					weightSum = weightSum + weight;
-				}
-			}
-
-			weightSum = weightSum * 15.0f;
 
 			// finalWeight is weight for 1 pixel on Sphere
 			// S = 4 * PI * R^2
@@ -184,8 +167,8 @@ namespace Skylicht
 
 			m_radiance->unlock();
 
-			/*
 			// Test SH
+			/*
 			u8 *testData = new u8[RT_SIZE * RT_SIZE * bpp];
 			for (u32 y = 0; y < RT_SIZE; y++)
 			{
@@ -194,31 +177,28 @@ namespace Skylicht
 				for (u32 x = 0; x < RT_SIZE; x++)
 				{
 					float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
-					float v = -((y / float(RT_SIZE)) * 2.0f - 1.0f);
+					float v = ((y / float(RT_SIZE)) * 2.0f - 1.0f);
 
 					dirTS.X = u;
 					dirTS.Y = v;
 					dirTS.Z = 1.0f;
-					toTangentSpace[4].rotateVect(dirTS);
+					toTangentSpace[0].rotateVect(dirTS);
 					dirTS.normalize();
 
 					getSHColor(dirTS, m_sh9, color);
+					color.X = core::clamp(color.X, 0.0f, 1.0f);
+					color.Y = core::clamp(color.Y, 0.0f, 1.0f);
+					color.Z = core::clamp(color.Z, 0.0f, 1.0f);
 
-					color.X = core::clamp(color.X * 255.0f, 0.0f, 255.0f);
-					color.Y = core::clamp(color.Y * 255.0f, 0.0f, 255.0f);
-					color.Z = core::clamp(color.Z * 255.0f, 0.0f, 255.0f);
-
-					data[0] = (u8)(color.X);
-					data[1] = (u8)(color.Y);
-					data[2] = (u8)(color.Z);
+					data[0] = (u8)(color.X * 255.0f);
+					data[1] = (u8)(color.Y * 255.0f);
+					data[2] = (u8)(color.Z * 255.0f);
 					data[3] = 255;
 
 					data += bpp;
 				}
 			}
-			*/
 
-			/*
 			IImage* im = driver->createImageFromData(
 				video::ECF_A8R8G8B8,
 				core::dimension2du(RT_SIZE, RT_SIZE),
@@ -227,10 +207,10 @@ namespace Skylicht
 			if (driver->getDriverType() == video::EDT_DIRECT3D11)
 				im->swapBG();
 
-			driver->writeImageToFile(im, "C:\\SVN\\testsh.png");
+			driver->writeImageToFile(im, "D:\\testsh.png");
 			im->drop();
 
-			CBaseRP::saveFBOToFile(m_radiance, "C:\\SVN\\test.png");
+			CBaseRP::saveFBOToFile(m_radiance, "D:\\test.png");
 
 			delete testData;
 			*/
@@ -238,42 +218,41 @@ namespace Skylicht
 
 		void CRenderToTexture::projectOntoSH(const core::vector3df& n, const core::vector3df& color, core::vector3df *sh)
 		{
-			// Cosine kernel
-			const float A0 = 3.141593f;
-			const float A1 = 2.095395f;
-			const float A2 = 0.785398f;
+			// Cosine kernel for SH
+			const float A0 = 1.0f;// core::PI;
+			const float A1 = 1.0f;// (2.0f * core::PI) / 3.0f;
+			const float A2 = 1.0f;// core::PI / 4.0f;
 
+			// https://github.com/TheRealMJP/LowResRendering/blob/master/SampleFramework11/v1.01/Graphics/SH.cpp			
 			// Band 0
-			sh[0] = sh[0] + 0.282095f * A0 * color;
+			sh[0] = 0.282095f * color * A0;
 
 			// Band 1
-			sh[1] = sh[1] + 0.488603f * n.X * A1 * color;
-			sh[2] = sh[2] + 0.488603f * n.Y * A1 * color;
-			sh[3] = sh[3] + 0.488603f * n.Z * A1 * color;
+			sh[1] = 0.488603f * n.Y * color * A1;
+			sh[2] = 0.488603f * n.Z * color * A1;
+			sh[3] = 0.488603f * n.X * color * A1;
 
 			// Band 2
-			sh[4] = sh[4] + 1.092548f * n.X * n.Z * A2 * color;
-			sh[5] = sh[5] + 1.092548f * n.Z * n.Y * A2 * color;
-			sh[6] = sh[7] + 1.092548f * n.Y * n.X * A2 * color;
+			sh[4] = 1.092548f * n.X * n.Y * color * A2;
+			sh[5] = 1.092548f * n.Y * n.Z * color * A2;
+			sh[6] = 0.315392f * (3.0f * n.Z * n.Z - 1.0f) * color * A2;
 
-			sh[7] = sh[6] + 0.315392f * (3.0f * n.Z * n.Z - 1.0f) * A2 * color;
-			sh[8] = sh[8] + 0.546274f * (n.X * n.X - n.Y * n.Y) * A2 * color;
+			sh[7] = 1.092548f * n.X * n.Z * color * A2;
+			sh[8] = 0.546274f * (n.X * n.X - n.Y * n.Y) * color * A2;
 		}
 
 		void CRenderToTexture::getSHColor(const core::vector3df& n, const core::vector3df *sh, core::vector3df& color)
 		{
-			color = (
-				sh[0] +
+			// EvalSH9
+			// https://github.com/TheRealMJP/BakingLab/blob/master/SampleFramework11/v1.02/Shaders/SH.hlsl
+			core::vector3df dirSH[9];
+			projectOntoSH(n, core::vector3df(1.0f, 1.0f, 1.0f), dirSH);
 
-				sh[1] * n.X +
-				sh[2] * n.Y +
-				sh[3] * n.Z +
-
-				sh[4] * n.Z * n.X +
-				sh[5] * n.Y * n.Z +
-				sh[6] * n.Y * n.X +
-				sh[7] * (3.0f * n.Z * n.Z - 1.0f) +
-				sh[8] * (n.X*n.X - n.Y * n.Y));
+			color.set(0.0f, 0.0f, 0.0f);
+			for (int i = 0; i < 9; i++)
+			{
+				color = color + dirSH[i] * sh[i];
+			}
 		}
 
 		void CRenderToTexture::getWorldView(
