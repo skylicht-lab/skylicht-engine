e57f7eb7ce0c0ba66f4d05a8038ab815a0f58027
ducphamhong <hongduc.pr@gmail.com>
2020-05-21 21:12:46 +0700

    feat: #71 Optimize 100% time bake indirect

--- Projects/Skylicht/Lightmapper/Source/Lightmapper/CMTBaker.cpp
+++ Projects/Skylicht/Lightmapper/Source/Lightmapper/CMTBaker.cpp
@@ -13,11 +13,25 @@ namespace Skylicht
 {
 	namespace Lightmapper
 	{
-		CMTBaker::CMTBaker()
+		CMTBaker::CMTBaker() :
+			m_weightSum(0.0f)
 		{
 			IVideoDriver *driver = getVideoDriver();
 			core::dimension2du s(RT_SIZE * NUM_FACES, RT_SIZE * NUM_MTBAKER);
 			m_radiance = driver->addRenderTargetTexture(s, "lmrt", video::ECF_A8R8G8B8);
+
+			for (int y = 0; y < RT_SIZE; y++)
+			{
+				for (int x = 0; x < RT_SIZE; x++)
+				{
+					float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
+					float v = -((y / float(RT_SIZE)) * 2.0f - 1.0f);
+
+					float temp = 1.0f + u * u + v * v;
+					float weight = 4.0f / (sqrt(temp) * temp);
+					m_weightSum = m_weightSum + weight;
+				}
+			}
 		}
 
 		CMTBaker::~CMTBaker()
@@ -98,69 +112,79 @@ namespace Skylicht
 			if (getVideoDriver()->getDriverType() == video::EDT_OPENGL)
 				isBGR = true;
 
+#pragma omp parallel for
 			for (int tid = 0; tid < count; tid++)
-			{
 				m_sh[tid].zero();
 
-				// Compute the final weight for integration
-				float weightSum = 0.0f;
+			int height = RT_SIZE * count;
+			int width = RT_SIZE * numFace;
 
-				// Compute SH by radiance (use OpenMP)
-				for (int face = 0; face < numFace; face++)
+			u8 *faceData = NULL;
+			u8 *data = NULL;
+			int x = 0;
+			int y = 0;
+			int face = 0;
+			int tid = 0;
+
+			// Compute SH by radiance (use OpenMP)
+#pragma omp parallel for private(dirTS, color, data, x, y, face, tid)
+			for (int imgy = 0; imgy < height; imgy++)
+			{
+				for (int imgx = 0; imgx < width; imgx++)
 				{
-					// scan pixels
-					// offset to face data
-					u8 *faceData = imageData + RT_SIZE * face * bpp;
-					u8 *data = NULL;
+					// calc face & tid
+					face = imgx / RT_SIZE;
+					tid = imgy / RT_SIZE;
 
-#pragma omp parallel for private(dirTS) private(color) private(data)
-					for (int y = 0; y < RT_SIZE; y++)
+					y = imgy - tid * RT_SIZE;
+					x = imgx - face * RT_SIZE;
+
+					// face data
+					faceData = imageData + RT_SIZE * face * bpp;
+
+					// offset to tid and row y
+					data = faceData + (RT_SIZE * tid + y) * rowSize;
+
+					// offset to pixel x 
+					data += x * bpp;
+
+					// Calculate the location in [-1, 1] texture space
+					float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
+					float v = -((y / float(RT_SIZE)) * 2.0f - 1.0f);
+
+					float temp = 1.0f + u * u + v * v;
+					float weight = 4.0f / (sqrt(temp) * temp);
+
+					if (isBGR == true)
 					{
-						for (int x = 0; x < RT_SIZE; x++)
-						{
-							// offset to tid and row y
-							data = faceData + (RT_SIZE * tid + y) * rowSize;
-
-							// offset to pixel x 
-							data += x * bpp;
-
-							// Calculate the location in [-1, 1] texture space
-							float u = ((x / float(RT_SIZE)) * 2.0f - 1.0f);
-							float v = -((y / float(RT_SIZE)) * 2.0f - 1.0f);
-
-							float temp = 1.0f + u * u + v * v;
-							float weight = 4.0f / (sqrt(temp) * temp);
-							weightSum = weightSum + weight;
-
-							if (isBGR == true)
-							{
-								color.X = data[2] * c * weight; // b
-								color.Y = data[1] * c * weight; // g
-								color.Z = data[0] * c * weight; // r
-							}
-							else
-							{
-								color.X = data[0] * c * weight; // r
-								color.Y = data[1] * c * weight; // g
-								color.Z = data[2] * c * weight; // b
-							}
-
-							dirTS.X = u;
-							dirTS.Y = v;
-							dirTS.Z = 1.0f;
-							toTangentSpace[tid][face].rotateVect(dirTS);
-							dirTS.normalize();
-
-							m_sh[tid].projectAddOntoSH(dirTS, color);
-						}
+						color.X = data[2] * c * weight; // b
+						color.Y = data[1] * c * weight; // g
+						color.Z = data[0] * c * weight; // r
 					}
-				}
+					else
+					{
+						color.X = data[0] * c * weight; // r
+						color.Y = data[1] * c * weight; // g
+						color.Z = data[2] * c * weight; // b
+					}
 
-				// finalWeight is weight for 1 pixel on Sphere
-				// S = 4 * PI * R^2
-				float finalWeight = (4.0f * 3.14159f) / weightSum;
-				m_sh[tid] *= finalWeight;
+					dirTS.X = u;
+					dirTS.Y = v;
+					dirTS.Z = 1.0f;
+					toTangentSpace[tid][face].rotateVect(dirTS);
+					dirTS.normalize();
+
+					m_sh[tid].projectAddOntoSH(dirTS, color);
+				}
 			}
+
+			// finalWeight is weight for 1 pixel on Sphere
+			// S = 4 * PI * R^2
+			float finalWeight = (4.0f * 3.14159f) / (m_weightSum * numFace);
+
+#pragma omp parallel for
+			for (int tid = 0; tid < count; tid++)
+				m_sh[tid] *= finalWeight;
 
 			m_radiance->unlock();
 
