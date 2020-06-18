2cfc5f454d272bcc8ccaf310ddc04f8e07bad16c
ducphamhong <hongduc.pr@gmail.com>
2020-06-18 22:56:57 +0700

    feat: #54 Add rasterisation, update defferred shader

--- Samples/Lightmapping/Source/CViewInit.cpp
+++ Samples/Lightmapping/Source/CViewInit.cpp
@@ -1,11 +1,9 @@
 #include "pch.h"
 #include "CViewInit.h"
 #include "CViewBakeLightmap.h"
-
-#include "ViewManager/CViewManager.h"
 #include "Context/CContext.h"
+#include "ViewManager/CViewManager.h"
 
-#include "GridPlane/CGridPlane.h"
 #include "SkyDome/CSkyDome.h"
 
 CViewInit::CViewInit() :
@@ -29,10 +27,18 @@ io::path CViewInit::getBuiltInPath(const char *name)
 
 void CViewInit::onInit()
 {
-	getApplication()->getFileSystem()->addFileArchive(getBuiltInPath("BuiltIn.zip"), false, false);
+	// init application
+	CBaseApp* app = getApplication();
 
+	// load "BuiltIn.zip" to read files inside it
+	app->getFileSystem()->addFileArchive(app->getBuiltInPath("BuiltIn.zip"), false, false);
+	app->getFileSystem()->addFileArchive(app->getBuiltInPath("Common.zip"), false, false);
+	app->getFileSystem()->addFileArchive(app->getBuiltInPath("LightmapUV.zip"), false, false);
+
+	// load basic shader
 	CShaderManager *shaderMgr = CShaderManager::getInstance();
 	shaderMgr->initBasicShader();
+	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Deferred/Color.xml");
 	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Deferred/DiffuseNormal.xml");
 	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Deferred/Specular.xml");
 	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Deferred/Diffuse.xml");
@@ -42,18 +48,19 @@ void CViewInit::onInit()
 	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Lighting/SGPointLight.xml");
 	shaderMgr->loadShader("BuiltIn/Shader/SpecularGlossiness/Lighting/SGPointLightShadow.xml");
 
-	CGlyphFreetype *freetypeFont = CGlyphFreetype::getInstance();
-	freetypeFont->initFont("Segoe UI Light", "BuiltIn/Fonts/segoeui/segoeuil.ttf");
-}
+	// create a Scene
+	CContext *context = CContext::getInstance();
+	CScene *scene = context->initScene();
 
-void CViewInit::initScene()
-{
-	CBaseApp *app = getApplication();
-
-	CScene *scene = CContext::getInstance()->initScene();
+	// create a Zone in Scene
 	CZone *zone = scene->createZone();
 
-	// camera
+	// camera 2D
+	CGameObject *guiCameraObject = zone->createEmptyObject();
+	CCamera* guiCamera = guiCameraObject->addComponent<CCamera>();
+	guiCamera->setProjectionType(CCamera::OrthoUI);
+
+	// camera 3D
 	CGameObject *camObj = zone->createEmptyObject();
 	camObj->addComponent<CCamera>();
 	camObj->addComponent<CEditorCamera>()->setMoveSpeed(2.0f);
@@ -61,12 +68,6 @@ void CViewInit::initScene()
 	CCamera *camera = camObj->getComponent<CCamera>();
 	camera->setPosition(core::vector3df(-10.0f, 5.0f, 10.0f));
 	camera->lookAt(core::vector3df(0.0f, 0.0f, 0.0f), core::vector3df(0.0f, 1.0f, 0.0f));
-
-	// gui camera
-	CGameObject *guiCameraObj = zone->createEmptyObject();
-	guiCameraObj->addComponent<CCamera>();
-	CCamera *guiCamera = guiCameraObj->getComponent<CCamera>();
-	guiCamera->setProjectionType(CCamera::OrthoUI);
 
 	// sky
 	ITexture *skyDomeTexture = CTextureManager::getInstance()->getTexture("Common/Textures/Sky/PaperMill.png");
@@ -82,55 +83,36 @@ void CViewInit::initScene()
 	CTransformEuler *lightTransform = lightObj->getTransformEuler();
 	lightTransform->setPosition(core::vector3df(2.0f, 2.0f, 2.0f));
 
-	core::vector3df direction = core::vector3df(-2.0f, -7.0f, -1.5f);
+	core::vector3df direction = core::vector3df(4.0f, -6.0f, -4.5f);
 	lightTransform->setOrientation(direction, CTransform::s_oy);
 
-	CMeshManager *meshManager = CMeshManager::getInstance();
-	CEntityPrefab *prefab = NULL;
+	// gazebo object
+	CEntityPrefab *model = CMeshManager::getInstance()->loadModel("LightmapUV/gazebo.smesh", "LightmapUV");
 
-	std::vector<std::string> textureFolders;
-	textureFolders.push_back("Sponza/Textures");
-
-	// load model
-	prefab = meshManager->loadModel("TankScene/TankScene.obj", NULL, true);
-	if (prefab != NULL)
+	if (model != NULL)
 	{
-		// export model material if TankScene.xml is not exist
-		// CMaterialManager::getInstance()->exportMaterial(prefab, "../Assets/TankScene/TankScene.xml");
+		CGameObject *gazeboObj = zone->createEmptyObject();
+		CRenderMesh *renderMesh = gazeboObj->addComponent<CRenderMesh>();
+		renderMesh->initFromPrefab(model);
 
-		// load material
-		ArrayMaterial& materials = CMaterialManager::getInstance()->loadMaterial("TankScene/TankScene.xml", true, textureFolders);
+		ArrayMaterial materials = CMaterialManager::getInstance()->initDefaultMaterial(model);
+		for (CMaterial *material : materials)
+			material->changeShader("BuiltIn/Shader/SpecularGlossiness/Deferred/Color.xml");
 
-		// todo modify material
-		//for (CMaterial* m : materials)
-		//{
-		//	m->changeShader("BuiltIn/Shader/SpecularGlossiness/Deferred/Diffuse.xml");
-		//	m->autoDetectLoadTexture();
-		//	m->deleteExtramParams();
-		//}
-
-		// and save material
-		// CMaterialManager::getInstance()->saveMaterial(materials, "../Assets/TankScene/TankScene1.xml");
-
-		// create render mesh object
-		CGameObject *tankScene = zone->createEmptyObject();
-		tankScene->setStatic(true);
-
-		CRenderMesh *renderer = tankScene->addComponent<CRenderMesh>();
-		renderer->initFromPrefab(prefab);
-		renderer->initMaterial(materials);
-
-		CIndirectLighting *indirectLighting = tankScene->addComponent<CIndirectLighting>();
-		indirectLighting->setIndirectLightingType(CIndirectLighting::VertexColor);
+		renderMesh->initMaterial(materials);
 	}
 
-	// save to context
-	CContext *context = CContext::getInstance();
+	// save to context	
 	context->initRenderPipeline(app->getWidth(), app->getHeight());
 	context->setActiveZone(zone);
 	context->setActiveCamera(camera);
 	context->setGUICamera(guiCamera);
 	context->setDirectionalLight(directionalLight);
+}
+
+void CViewInit::initScene()
+{
+
 }
 
 void CViewInit::onDestroy()
@@ -149,8 +131,8 @@ void CViewInit::onUpdate()
 		io::IFileSystem* fileSystem = getApplication()->getFileSystem();
 
 		std::vector<std::string> listBundles;
-		listBundles.push_back("Common.Zip");
-		listBundles.push_back("TankScene.Zip");
+		listBundles.push_back("Common.zip");
+		listBundles.push_back("LightmapUV.zip");
 
 #ifdef __EMSCRIPTEN__
 		const char *filename = listBundles[m_downloaded].c_str();
@@ -244,7 +226,7 @@ void CViewInit::onUpdate()
 	}
 	break;
 	}
-}
+	}
 
 void CViewInit::onRender()
 {
