6aa8dfb70391ec650ec349948d322ccaf838dc2f
ducphamhong <hongduc.pr@gmail.com>
2020-07-02 10:51:10 +0700

    Add sample materials

---
+++ Samples/Materials/Source/SampleMaterials.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "IApplicationEventReceiver.h"
+
+class SampleMaterials : public IApplicationEventReceiver
+{
+private:
+	CScene *m_scene;
+	CCamera *m_guiCamera;
+
+public:
+	SampleMaterials();
+	virtual ~SampleMaterials();
+
+	virtual void onUpdate();
+
+	virtual void onRender();
+
+	virtual void onPostRender();
+
+	virtual void onResume();
+
+	virtual void onPause();
+
+	virtual bool onBack();
+
+	virtual void onInitApp();
+
+	virtual void onQuitApp();
+};
