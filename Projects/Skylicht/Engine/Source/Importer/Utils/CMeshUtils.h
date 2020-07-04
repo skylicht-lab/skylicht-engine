45428155c82ade8bdfd5a498d51e3c73b244175d
ducphamhong <hongduc.pr@gmail.com>
2020-07-04 19:20:07 +0700

    Fix flip Y normal Update specular LitFS

--- Projects/Skylicht/Engine/Source/Importer/Utils/CMeshUtils.h
+++ Projects/Skylicht/Engine/Source/Importer/Utils/CMeshUtils.h
@@ -31,10 +31,10 @@ namespace Skylicht
 	public:
 		static void copyVertices(IVertexBuffer *srcBuffer, IVertexBuffer *dstBuffer);
 
-		static void convertToTangentVertices(IMeshBuffer* buffer, bool flipZUp = false);
+		static void convertToTangentVertices(IMeshBuffer* buffer, bool fixFlipY = true);
 
 		static void convertToSkinVertices(IMeshBuffer* buffer);
 
-		static void convertToSkinTangentVertices(IMeshBuffer* buffer, bool flipZUp = false);
+		static void convertToSkinTangentVertices(IMeshBuffer* buffer, bool fixFlipY = true);
 	};
 }
