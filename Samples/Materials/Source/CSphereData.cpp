354b205b22368fe21f8da9b5b1a8993f6def89c3
ducphamhong <hongduc.pr@gmail.com>
2020-07-04 11:31:13 +0700

    Update demo shader material

---
+++ Samples/Materials/Source/CSphereData.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "SkylichtEngine.h"
+#include "CSphereData.h"
+
+CSphereData::CSphereData() :
+	SphereMeshBuffer(NULL)
+{
+
+}
+
+CSphereData::~CSphereData()
+{
+	if (SphereMeshBuffer != NULL)
+		SphereMeshBuffer->drop();
+}
+
+void CSphereData::initSphere(float radius, CMaterial *material)
+{
+	if (SphereMeshBuffer != NULL)
+		SphereMeshBuffer->drop();
+
+	if (RenderMesh != NULL)
+		RenderMesh->drop();
+
+	RenderMesh = new CMesh();
+
+	IMesh *mesh = getIrrlichtDevice()->getSceneManager()->getGeometryCreator()->createSphereMesh(radius);
+
+	SphereMeshBuffer = mesh->getMeshBuffer(0);
+	CMeshUtils::convertToTangentVertices(SphereMeshBuffer);
+
+	// assign mesh buffer material
+	material->addAffectMesh(SphereMeshBuffer);
+
+	// recalc bbox
+	SphereMeshBuffer->recalculateBoundingBox();
+
+	// add cube mesh buffer to mesh
+	RenderMesh->addMeshBuffer(SphereMeshBuffer, material->getName(), material);
+
+	// recalc bbox for culling
+	RenderMesh->recalculateBoundingBox();
+
+	// remeber set static mesh buffer to optimize (it will stored on GPU)
+	RenderMesh->setHardwareMappingHint(EHM_STATIC);
+
+	// hold this pointer: 3 holder (RenderMesh, CSphereData, IMesh);
+	SphereMeshBuffer->grab();
+
+	// release memory
+	mesh->drop();
+}
