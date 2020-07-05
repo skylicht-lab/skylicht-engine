f9da478e1fbd7c257e78585d51d997129ee089f2
ducphamhong <hongduc.pr@gmail.com>
2020-07-05 23:25:12 +0700

    Fix missing file, build error

---
+++ Samples/Shader/Source/CRotateComponent.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "SkylichtEngine.h"
+#include "CRotateComponent.h"
+
+CRotateComponent::CRotateComponent()
+{
+
+}
+
+CRotateComponent::~CRotateComponent()
+{
+
+}
+
+void CRotateComponent::initComponent()
+{
+
+}
+
+void CRotateComponent::updateComponent()
+{
+	CTransformEuler *transform = m_gameObject->getTransformEuler();
+
+	float timestep = getTimeStep();
+	core::vector3df rot = transform->getRotation();
+
+	rot.X = rot.X + m_rotateSpeed.X * timestep;
+	rot.Y = rot.Y + m_rotateSpeed.Y * timestep;
+	rot.Z = rot.Z + m_rotateSpeed.Z * timestep;
+
+	rot.X = fmodf(rot.X, 360.0f);
+	rot.Y = fmodf(rot.Y, 360.0f);
+	rot.Z = fmodf(rot.Z, 360.0f);
+
+	transform->setRotation(rot);
+}
