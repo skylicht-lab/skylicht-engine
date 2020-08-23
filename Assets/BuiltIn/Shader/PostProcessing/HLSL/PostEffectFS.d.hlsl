ccccaef6925abe55f37497a614ee2a395d26f2e5
ducphamhong <hongduc.pr@gmail.com>
2020-08-23 12:14:57 +0700

    feat: #86 Add camera exposure shader

--- Assets/BuiltIn/Shader/PostProcessing/HLSL/PostEffectFS.d.hlsl
+++ Assets/BuiltIn/Shader/PostProcessing/HLSL/PostEffectFS.d.hlsl
@@ -1,11 +1,8 @@
 Texture2D uTexColor : register(t0);
 SamplerState uTexColorSampler : register(s0);
 
-Texture2D uTexCurrentLum : register(t1);
-SamplerState uTexCurrentLumSampler : register(s1);
-
-Texture2D uTexLastLum : register(t2);
-SamplerState uTexLastLumSampler : register(s2);
+Texture2D uTexLuminance : register(t1);
+SamplerState uTexLuminanceSampler : register(s1);
 
 struct PS_INPUT
 {
@@ -16,39 +13,18 @@ struct PS_INPUT
 
 #include "LibToneMapping.hlsl"
 
-float CalcLuminance(float3 color)
-{
-    return max(dot(color, float3(0.299, 0.587, 0.114)), 0.0001);
-}
-
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	float4 color = uTexColor.Sample(uTexColorSampler, input.tex0);
 	
-	float currentLum = uTexCurrentLum.SampleLevel(uTexCurrentLumSampler, input.tex0, 10.0).x;
-	// float lastLum = uTexLastLum.SampleLevel(uTexLastLumSampler, input.tex0, 10.0).x;
+	float avgLuminance = uTexLuminance.SampleLevel(uTexLuminanceSampler, input.tex0, 10.0).x;
 	
-	if (input.tex0.x < 0.5)
-	{		
-		float c = currentLum;
-		return float4(c, c, c, 1.0);
-	}	
+	float target = 0.25;
+	float threshold = 2.5;
+	float linearExposure = max((target / avgLuminance), 0.0001);
 	
-	/*
-	float adaptedLum = lastLum + (currentLum - lastLum) * 0.1;
-	float pixelLuminance = CalcLuminance(color.rgb);
+	// Function: exposure = exp(log(target/x)) see function graph for more detail
+	float exposure = min(exp2(log2(linearExposure)), threshold);
 	
-	float avgLuminance = max(adaptedLum, 0.001f);
-	
-	float keyValue = 0.2;
-	
-	float linearExposure = (keyValue / avgLuminance);
-	
-	float exposure = log2(max(linearExposure, 0.0001f));
-    // exposure -= threshold;
-    
-	color = exp2(exposure) * color;
-	*/
-	
-	return float4(linearRGB(color.rgb), color.a);
+	return float4(linearRGB(exposure * color.rgb), color.a);
 }
