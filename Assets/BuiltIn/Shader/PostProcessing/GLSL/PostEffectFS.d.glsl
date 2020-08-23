482b7188bffec861fa526ad5d4f448763eb2abbf
ducphamhong <hongduc.pr@gmail.com>
2020-08-23 12:14:57 +0700

    feat: #86 Add camera exposure shader

--- Assets/BuiltIn/Shader/PostProcessing/GLSL/PostEffectFS.d.glsl
+++ Assets/BuiltIn/Shader/PostProcessing/GLSL/PostEffectFS.d.glsl
@@ -3,8 +3,7 @@ precision highp sampler2D;
 precision highp sampler2DArray;
 
 uniform sampler2D uTexColor;
-uniform sampler2D uTexCurrentLum;
-uniform sampler2D uTexLastLum;
+uniform sampler2D uTexLuminance;
 
 in vec2 varTexCoord0;
 in vec4 varColor;
@@ -16,14 +15,13 @@ void main(void)
 {
 	vec4 color = texture(uTexColor, varTexCoord0.xy);
 	
-	float currentLum = textureLod(uTexCurrentLum, varTexCoord0.xy, 10.0).x;
+	float avgLuminance = textureLod(uTexLuminance, varTexCoord0.xy, 10.0).x;			
+		
+	float target = 0.25;
+	float threshold = 2.5;
+	float linearExposure = max((target / avgLuminance), 0.0001);
 	
-	if (varTexCoord0.x < 0.5)
-	{		
-		float c = currentLum;
-		FragColor = vec4(c, c, c, 1.0);
-		return;
-	}
-	
-	FragColor = vec4(linearRGB(color.rgb), color.a);
+	float exposure = min(exp2(log2(linearExposure)), threshold);
+		
+	FragColor = vec4(linearRGB(exposure * color.rgb), color.a);
 }
