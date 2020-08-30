16d7e84f4f7620c51074b02aaa648ffe60469c52
ducphamhong <hongduc.pr@gmail.com>
2020-08-30 12:08:34 +0700

    feat: #102 Implement particle system update

--- Projects/Skylicht/Components/Source/ParticleSystem/Particles/CGroup.cpp
+++ Projects/Skylicht/Components/Source/ParticleSystem/Particles/CGroup.cpp
@@ -23,7 +23,10 @@ https://github.com/skylicht-lab/skylicht-engine
 */
 
 #include "pch.h"
+
 #include "CGroup.h"
+
+#include "Systems/CParticleSystem.h"
 
 namespace Skylicht
 {
@@ -31,19 +34,110 @@ namespace Skylicht
 	{
 		CGroup::CGroup() :
 			m_zone(NULL),
-			m_renderer(NULL)
+			m_renderer(NULL),
+			Gravity(0.0f, -1.0f, 0.0f),
+			Friction(0.1f),
+			LifeMin(0.5f),
+			LifeMax(1.0f)
 		{
-
+			m_defaultSystem = new CParticleSystem();
 		}
 
 		CGroup::~CGroup()
 		{
-
+			delete m_defaultSystem;
 		}
 
 		void CGroup::update()
 		{
+			if (m_zone == NULL)
+				return;
 
+			float dt = getTimeStep();
+
+			u32 autoBorn = 0;
+
+			// update emitter
+			m_launch.set_used(0);
+			for (CEmitter *e : m_emitters)
+			{
+				u32 nb = e->updateNumber(dt);
+				if (nb > 0)
+				{
+					SLaunchParticle data = { e, nb };
+					m_launch.push_back(data);
+					autoBorn += nb;
+				}
+			}
+
+			CParticle *particles = m_particles.pointer();
+			u32 num = m_particles.size();
+
+			// update particle system
+			m_defaultSystem->update(particles, num, this, dt);
+			for (ISystem *s : m_systems)
+				s->update(particles, num, this, dt);
+
+
+			u32 emiterId = 0;
+			u32 emiterLaunch = m_launch.size();
+
+			// launch new and remove die particle
+			for (u32 i = 0; i < num; i++)
+			{
+				CParticle& p = particles[i];
+				if (p.Life < 0)
+				{
+					// try replace to optimize
+					if (autoBorn > 0)
+					{
+						p.Age = 0.0f;
+						p.Life = LifeMin + (LifeMax - LifeMin) * os::Randomizer::frand();
+
+						if (launchParticle(p, m_launch[emiterId]) == true)
+							emiterId++;
+
+						--autoBorn;
+					}
+					else
+					{
+						remove(i);
+						--i;
+					}
+				}
+			}
+
+			// born new particle
+			if (autoBorn > 0)
+			{
+				for (u32 i = emiterId; i < emiterLaunch; i++)
+				{
+					SLaunchParticle &launch = m_launch[i];
+					if (launch.Number > 0)
+					{
+						CParticle* particles = create(launch.Number);
+
+						for (u32 j = 0; j < launch.Number; j++)
+						{
+							CParticle &p = particles[j];
+							p.Age = 0.0f;
+							p.Life = LifeMin + (LifeMax - LifeMin) * os::Randomizer::frand();
+
+							launch.Emitter->emitParticle(p, m_zone);
+						}
+
+						launch.Number = 0;
+					}
+				}
+			}
+		}
+
+		bool CGroup::launchParticle(CParticle& p, SLaunchParticle& launch)
+		{
+			launch.Emitter->emitParticle(p, m_zone);
+			launch.Number--;
+
+			return launch.Number == 0;
 		}
 
 		CParticle* CGroup::create(u32 num)
