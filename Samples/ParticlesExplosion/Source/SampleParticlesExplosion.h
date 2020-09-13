4dc89657436453b3687a6376d7be56ab7acf2060
ducphamhong <hongduc.pr@gmail.com>
2020-09-13 17:51:40 +0700

    feat: #102 Add project demo explosion

---
+++ Samples/ParticlesExplosion/Source/SampleParticlesExplosion.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "IApplicationEventReceiver.h"
+#include "ParticleSystem/CParticleComponent.h"
+
+#define MAX_POOL 20
+
+class SampleParticlesExplosion :
+	public IApplicationEventReceiver,
+	public IEventReceiver
+{
+private:
+	CScene *m_scene;
+	CCamera *m_guiCamera;
+	CCamera *m_camera;
+
+	CForwardRP *m_forwardRP;
+
+	CGlyphFont* m_font;
+
+	Particle::CParticleComponent* m_particlePool[MAX_POOL];
+
+public:
+	SampleParticlesExplosion();
+
+	virtual ~SampleParticlesExplosion();
+
+	virtual bool OnEvent(const SEvent& event);
+
+	void initParticleSystem(Particle::CParticleComponent *ps);
+
+	virtual void onUpdate();
+
+	virtual void onRender();
+
+	virtual void onPostRender();
+
+	virtual void onResume();
+
+	virtual void onPause();
+
+	virtual bool onBack();
+
+	virtual void onInitApp();
+
+	virtual void onQuitApp();
+};
