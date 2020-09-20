03c3cd5c7c8d29b606842c34daa5790d17675b3a
ducphamhong <hongduc.pr@gmail.com>
2020-09-20 16:26:58 +0700

    feat: #102 Add test noise 3d project

---
+++ Assets/BuiltIn/Shader/Noise/GLSL/Noise3DVS.glsl
@@ -0,0 +1,15 @@
+in vec4 inPosition;
+in vec4 inColor;
+
+uniform mat4 uMvpMatrix;
+uniform mat4 uWorld;
+
+out vec4 varColor;
+out vec4 varWorldPos;
+
+void main(void)
+{
+	varColor = inColor/255.0;
+	varWorldPos = uWorld * inPosition;
+	gl_Position = uMvpMatrix * inPosition;
+}
