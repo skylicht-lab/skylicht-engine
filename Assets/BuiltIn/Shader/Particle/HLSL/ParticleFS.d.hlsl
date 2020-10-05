2e3ef36b4a619e4f0434366933f0a8bc5dfe3f63
ducphamhong <hongduc.pr@gmail.com>
2020-10-05 21:32:26 +0700

    #86 Update shader noise, particle on linear color space

--- Assets/BuiltIn/Shader/Particle/HLSL/ParticleFS.d.hlsl
+++ Assets/BuiltIn/Shader/Particle/HLSL/ParticleFS.d.hlsl
@@ -1,21 +1,15 @@
 Texture2D uTexture : register(t0);
 SamplerState uTextureSampler : register(s0);
+
 struct PS_INPUT
 {
 	float4 pos : SV_POSITION;
 	float4 color : COLOR0;
 	float2 tex0 : TEXCOORD0;
 };
-static const float gamma = 2.2;
-static const float invGamma = 1.0/2.2;
-float3 sRGB(float3 color)
-{
-	return pow(color, gamma);
-}
-float3 linearRGB(float3 color)
-{
-	return pow(color, invGamma);
-}
+
+#include "../../PostProcessing/HLSL/LibToneMapping.hlsl"
+
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	float4 result = input.color * uTexture.Sample(uTextureSampler, input.tex0);
