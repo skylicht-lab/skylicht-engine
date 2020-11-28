70be367972d2539b970e3b5b460afd6f81551c88
ducphamhong <hongduc.pr@gmail.com>
2020-11-28 13:26:18 +0700

    #88 Add mp3 decoder

---
+++ Projects/Skylicht/Audio/Source/Stream/COnlineMemoryStream.h
@@ -0,0 +1,84 @@
+#ifndef _ONLINE_MEMORY_STREAM_H_
+#define _ONLINE_MEMORY_STREAM_H_
+
+#include "IStream.h"
+#include "Thread/IMutex.h"
+
+using namespace SkylichtSystem;
+
+namespace SkylichtAudio
+{
+	class COnlineMemoryStream;
+
+	class COnlineMemoryStreamCursor : public IStreamCursor
+	{
+	public:
+		int		m_pos;
+		COnlineMemoryStream		*m_stream;
+
+	public:
+		COnlineMemoryStreamCursor(COnlineMemoryStream *stream);
+		virtual ~COnlineMemoryStreamCursor();
+
+		virtual int seek(int pos, EOrigin origin);
+		virtual int tell();
+		virtual int read(unsigned char* buff, int len);
+		virtual bool endOfStream();
+		virtual int size();
+		virtual bool readyReadData(int len);
+	};
+
+	class COnlineMemoryStream : public IStream
+	{
+	protected:
+		unsigned char *m_buffer;
+		int m_size;
+		int m_allocSize;
+
+		IMutex *m_mutex;
+		bool m_fullDataStream;
+
+		bool m_stopStream;
+	public:
+		COnlineMemoryStream();
+		virtual ~COnlineMemoryStream();
+
+		virtual IStreamCursor* createCursor();
+
+		// remember lock stream mutex before run this function
+		unsigned char* getDataBuffer();
+
+		// remember lock stream mutex before run this function
+		int getDataBufferSize();
+
+		void uploadData(unsigned char *buffer, int size);
+
+		IMutex* getMutex()
+		{
+			return m_mutex;
+		}
+
+		void setFullData(bool b)
+		{
+			m_fullDataStream = b;
+		}
+
+		bool isFullData()
+		{
+			return m_fullDataStream;
+		}
+
+		bool isStopStream()
+		{
+			return m_stopStream;
+		}
+
+		void setStopStream(bool b)
+		{
+			m_stopStream = b;
+		}
+	};
+
+}
+
+#endif
--- Projects/ThirdParty/source/libmpg123/config.h
+++ Projects/ThirdParty/source/libmpg123/config.h
@@ -2,6 +2,9 @@
 #define OPT_GENERIC
 #define OPT_I386
 
+// disable ASM compile
+#define OPT_NO_OPTIMIZE
+
 #if defined(_WIN32)
 #include "config_window.h"
 #else
