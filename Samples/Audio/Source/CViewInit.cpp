c5eb8774922b9504f1b3f8dde32a1fa1d6621eb0
ducphamhong <hongduc.pr@gmail.com>
2020-11-28 16:53:07 +0700

    #88 Add sample play mp3/wav (not yet emscripten)

--- Samples/Audio/Source/CViewInit.cpp
+++ Samples/Audio/Source/CViewInit.cpp
@@ -11,6 +11,7 @@
 CViewInit::CViewInit() :
 	m_initState(CViewInit::DownloadBundles),
 	m_getFile(NULL),
+	m_spriteArchive(NULL),
 	m_downloaded(0)
 {
 
@@ -42,34 +43,33 @@ void CViewInit::onInit()
 
 void CViewInit::initScene()
 {
-	CBaseApp *app = getApplication();
+	CBaseApp* app = getApplication();
 
+	// load basic shader
+	CShaderManager *shaderMgr = CShaderManager::getInstance();
+	shaderMgr->initBasicShader();
+
+	// create a scene
 	CScene *scene = CContext::getInstance()->initScene();
 	CZone *zone = scene->createZone();
 
-	// camera
+	// create 2D camera
+	CGameObject *guiCameraObject = zone->createEmptyObject();
+	CCamera *guiCamera = guiCameraObject->addComponent<CCamera>();
+	guiCamera->setProjectionType(CCamera::OrthoUI);
+
+	// create 3D camera
 	CGameObject *camObj = zone->createEmptyObject();
 	camObj->addComponent<CCamera>();
 	camObj->addComponent<CEditorCamera>()->setMoveSpeed(2.0f);
-	camObj->addComponent<CFpsMoveCamera>()->setMoveSpeed(1.0f);
 
 	CCamera *camera = camObj->getComponent<CCamera>();
-	camera->setPosition(core::vector3df(-10.0f, 5.0f, 10.0f));
+	camera->setPosition(core::vector3df(0.0f, 1.5f, 4.0f));
 	camera->lookAt(core::vector3df(0.0f, 0.0f, 0.0f), core::vector3df(0.0f, 1.0f, 0.0f));
 
-	// gui camera
-	CGameObject *guiCameraObj = zone->createEmptyObject();
-	guiCameraObj->addComponent<CCamera>();
-	CCamera *guiCamera = guiCameraObj->getComponent<CCamera>();
-	guiCamera->setProjectionType(CCamera::OrthoUI);
-
-	// sky
-	ITexture *skyDomeTexture = CTextureManager::getInstance()->getTexture("Common/Textures/Sky/MonValley.png");
-	if (skyDomeTexture != NULL)
-	{
-		CSkyDome *skyDome = zone->createEmptyObject()->addComponent<CSkyDome>();
-		skyDome->setData(skyDomeTexture, SColor(255, 255, 255, 255));
-	}
+	// 3d grid
+	CGameObject *grid = zone->createEmptyObject();
+	grid->addComponent<CGridPlane>();
 
 	// lighting
 	CGameObject *lightObj = zone->createEmptyObject();
@@ -83,45 +83,19 @@ void CViewInit::initScene()
 	core::vector3df direction = core::vector3df(0.0f, -1.5f, 2.0f);
 	lightTransform->setOrientation(direction, CTransform::s_oy);
 
-	CMeshManager *meshManager = CMeshManager::getInstance();
-	CEntityPrefab *prefab = NULL;
+	// rendering pipe line
+	u32 w = app->getWidth();
+	u32 h = app->getHeight();
 
-	std::vector<std::string> textureFolders;
-	textureFolders.push_back("Sponza/Textures");
-
-	// Load model from TankScene.smesh
-	// How to export "TankScene.smesh" see SampleLightmappingVertex
-	prefab = meshManager->loadModel("TankScene/TankScene.smesh", NULL, true);
-	if (prefab != NULL)
-	{
-		// load material
-		ArrayMaterial& materials = CMaterialManager::getInstance()->loadMaterial("TankScene/TankScene.xml", true, textureFolders);
-
-		// create render mesh object
-		CGameObject *tankScene = zone->createEmptyObject();
-		tankScene->setStatic(true);
-
-		// render mesh & init material
-		CRenderMesh *renderer = tankScene->addComponent<CRenderMesh>();
-		renderer->initFromPrefab(prefab);
-		renderer->initMaterial(materials);
-
-		// set indirect lighting by VertexColor
-		// See SampleLightmappingVertex, that compute vertex color as indirect lighting
-		CIndirectLighting *indirectLighting = tankScene->addComponent<CIndirectLighting>();
-		indirectLighting->setIndirectLightingType(CIndirectLighting::VertexColor);
-	}
-
-	// save to context
 	CContext *context = CContext::getInstance();
-	context->initRenderPipeline(app->getWidth(), app->getHeight());
+	CForwardRP  *fwrp = new CForwardRP();
+	fwrp->initRender(w, h);
+
+	context->setCustomRP(fwrp);
 	context->setActiveZone(zone);
 	context->setActiveCamera(camera);
 	context->setGUICamera(guiCamera);
 	context->setDirectionalLight(directionalLight);
-
-	// adjust direct multipler
-	// context->getDefferredRP()->setDirectMultipler(0.6f);
 }
 
 void CViewInit::onDestroy()
@@ -140,9 +114,7 @@ void CViewInit::onUpdate()
 		io::IFileSystem* fileSystem = getApplication()->getFileSystem();
 
 		std::vector<std::string> listBundles;
-		listBundles.push_back("Common.Zip");
-		listBundles.push_back("TankScene.Zip");
-		listBundles.push_back("TankSceneDDS.Zip");
+		listBundles.push_back("SampleAudio.Zip");
 
 #ifdef __EMSCRIPTEN__
 		const char *filename = listBundles[m_downloaded].c_str();
@@ -198,6 +170,25 @@ void CViewInit::onUpdate()
 	break;
 	case CViewInit::InitScene:
 	{
+		if (m_spriteArchive != NULL)
+		{
+			m_sprite = new CSpriteAtlas(video::ECF_A8R8G8B8, 2048, 2048);
+
+			// get list sprite image
+			std::vector<std::string> sprites;
+
+			const io::IFileList *fileList = m_spriteArchive->getFileList();
+			for (int i = 0, n = fileList->getFileCount(); i < n; i++)
+			{
+				const char *fullFileame = fileList->getFullFileName(i).c_str();
+				const char *name = fileList->getFileName(i).c_str();
+
+				m_sprite->addFrame(name, fullFileame);
+			}
+
+			m_sprite->updateTexture();
+		}
+
 		initScene();
 		m_initState = CViewInit::Finished;
 	}
@@ -212,8 +203,6 @@ void CViewInit::onUpdate()
 		CScene *scene = context->getScene();
 		if (scene != NULL)
 			scene->update();
-
-		// CDeferredRP::enableTestIndirect(true);
 
 		CViewManager::getInstance()->getLayer(0)->changeView<CViewDemo>();
 	}
