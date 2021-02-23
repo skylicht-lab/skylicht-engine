f5acb6cdb07fb1203613d3f3d7a1022ff8c0b1f4
duc.pham <duc.pham@DESKTOP-47GPM7M>
2021-02-23 16:13:00 +0700

    #119 Add crypto library & generate Version.h

---
+++ Projects/Editor/Source/Version.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PROJECT_NAME "SkylichtEditor"
+#define PROJECT_VERSION "0.0.1"
---
+++ Scripts/Version.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PROJECT_NAME "@project_name@"
+#define PROJECT_VERSION "@project_version@"
