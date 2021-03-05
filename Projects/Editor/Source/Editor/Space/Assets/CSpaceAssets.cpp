4a6e636d5adc3cdcb51131f28800a7d64d0b9080
ducphamhong <hongduc.pr@gmail.com>
2021-03-05 22:26:25 +0700

    #119 Auto expand & focus tree folder

--- Projects/Editor/Source/Editor/Space/Assets/CSpaceAssets.cpp
+++ Projects/Editor/Source/Editor/Space/Assets/CSpaceAssets.cpp
@@ -51,18 +51,18 @@ namespace Skylicht
 
 			m_assetManager = CAssetManager::getInstance();
 
-			GUI::CTreeNode* root = m_folder->addNode(L"Assets", GUI::ESystemIcon::OpenFolder);
-			root->tagString(m_assetManager->getAssetFolder());
-			root->OnExpand = BIND_LISTENER(&CSpaceAssets::OnTreeNodeExpand, this);
-			root->OnCollapse = BIND_LISTENER(&CSpaceAssets::OnTreeNodeCollapse, this);
-			root->OnSelectChange = BIND_LISTENER(&CSpaceAssets::OnTreeNodeSelected, this);
-			root->expand();
+			m_root = m_folder->addNode(L"Assets", GUI::ESystemIcon::OpenFolder);
+			m_root->tagString(m_assetManager->getAssetFolder());
+			m_root->OnExpand = BIND_LISTENER(&CSpaceAssets::OnTreeNodeExpand, this);
+			m_root->OnCollapse = BIND_LISTENER(&CSpaceAssets::OnTreeNodeCollapse, this);
+			m_root->OnSelectChange = BIND_LISTENER(&CSpaceAssets::OnTreeNodeSelected, this);
+			m_root->expand(false);
 
 			std::vector<SFileInfo> files;
 
 			// add root to tree folder
 			m_assetManager->getRoot(files);
-			addTreeFolder(root, files);
+			addTreeFolder(m_root, files);
 
 			// column spliter
 			spliter->setControl(m_folder, 0, 0);
@@ -145,7 +145,7 @@ namespace Skylicht
 		{
 			m_listFiles->removeAllItem();
 
-			if (currentFolder.size() > 0 && 
+			if (currentFolder.size() > 0 &&
 				currentFolder != m_assetManager->getAssetFolder())
 			{
 				GUI::CListRowItem* item = m_listFiles->addItem(L"..", GUI::ESystemIcon::Folder);
@@ -188,10 +188,49 @@ namespace Skylicht
 					m_assetManager->getFolder(fullPath.c_str(), files);
 
 					addListFolder(fullPath, files);
+
+					expandTreeFolder(fullPath);
 				}
 				else
 				{
 					// shell open the file
+				}
+			}
+		}
+
+		void CSpaceAssets::expandTreeFolder(const std::string& folder)
+		{
+			std::string shortPath = m_assetManager->getShortPath(folder.c_str());
+			if (shortPath.size() > 0)
+			{
+				std::vector<std::string> result;
+				CStringImp::splitString(shortPath.c_str(), "/", result);
+
+				GUI::CTreeNode* node = m_root;
+
+				wchar_t wname[512];
+
+				for (u32 i = 0, n = result.size(); i < n; i++)
+				{
+					const std::string name = result[i];
+					CStringImp::convertUTF8ToUnicode(name.c_str(), wname);
+
+					GUI::CTreeNode* child = node->getChildNodeByLabel(wname);
+					if (child == NULL)
+						break;
+
+					if (child->haveChild())
+						child->expand(false);
+					else
+						child->expand(true);
+
+					node = child;
+				}
+
+				if (node != NULL)
+				{
+					node->forceLayout();
+					m_folder->getScrollControl()->scrollToItem(node);
 				}
 			}
 		}
