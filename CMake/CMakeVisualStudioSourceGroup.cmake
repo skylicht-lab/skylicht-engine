e8c7089d3e789d56b45bb8e2ff802b0cb45b3ba1
ducphamhong <hongduc.pr@gmail.com>
2021-03-21 11:45:34 +0700

    #6 Fix gitinorge & update editor Version.txt

--- CMake/CMakeVisualStudioSourceGroup.cmake
+++ CMake/CMakeVisualStudioSourceGroup.cmake
@@ -1,11 +1,11 @@
 # group source on visual project
-function(setup_project_group project_source current_dir)		
-	foreach(source IN LISTS project_source)		
+function(setup_project_group project_source current_dir)
+	foreach(source IN LISTS project_source)
 		# get source path
-		get_filename_component(source_path ${source} PATH)		
+		get_filename_component(source_path ${source} PATH)
 		
 		# get source relative path
-		string(REPLACE "${current_dir}/./" "" source_relative ${source_path})		
+		string(REPLACE "${current_dir}/./" "" source_relative ${source_path})
 		
 		if(MSVC OR XCODE)
 			# get group name
@@ -13,17 +13,17 @@ function(setup_project_group project_source current_dir)
 		else()
 			set(group_name ${source_relative})
 		endif()
-				
+		
 		if("${source_relative}" STREQUAL "${source_path}")
 			# no need group because source is in $current_dir
 		else()
 			# setup project group
 			source_group("${group_name}" FILES "${source}")
-		endif()		
+		endif()
 	endforeach()
 endfunction()
 
-function(add_source_group project_source group_name)		
+function(add_source_group project_source group_name)
 	foreach(source IN LISTS project_source)
 		# setup project group
 		source_group("${group_name}" FILES "${source}")
