b8fc6437165e3cb331cf7b598dc12c61de427c09
ducphamhong <hongduc.pr@gmail.com>
2021-03-21 11:45:34 +0700

    #6 Fix gitinorge & update editor Version.txt

--- CMake/CMakeVisualStudioPCH.cmake
+++ CMake/CMakeVisualStudioPCH.cmake
@@ -1,9 +1,9 @@
 function(target_precompiled_header project_target pch_source project_sources)
 if(MSVC)
-    get_filename_component(pch_basename ${pch_source} NAME_WE)
+	get_filename_component(pch_basename ${pch_source} NAME_WE)
 	set(pch_header "${pch_basename}.h")
 	
 	set_target_properties(${project_target} PROPERTIES COMPILE_FLAGS "/Yu${pch_header}")
-	set_source_files_properties(${pch_source} PROPERTIES COMPILE_FLAGS "/Yc${pch_header}")   
+	set_source_files_properties(${pch_source} PROPERTIES COMPILE_FLAGS "/Yc${pch_header}")
 endif(MSVC)
 endfunction()
