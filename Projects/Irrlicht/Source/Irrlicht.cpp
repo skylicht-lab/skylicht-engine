486592087e725de59fe9d868fb0048c946c17450
ducphamhong <hongduc.pr@gmail.com>
2021-07-09 23:02:55 +0700

    #125 Enable X11 for Linux

--- Projects/Irrlicht/Source/Irrlicht.cpp
+++ Projects/Irrlicht/Source/Irrlicht.cpp
@@ -8,28 +8,32 @@
 static const char* const copyright = "Irrlicht Engine (c) 2002-2012 Nikolaus Gebhardt";
 
 #ifdef _IRR_WINDOWS_
-	#include <windows.h>
-	#if defined(_DEBUG) && !defined(__GNUWIN32__)
-		#include <crtdbg.h>
-	#endif // _DEBUG
+#include <windows.h>
+#if defined(_DEBUG) && !defined(__GNUWIN32__)
+#include <crtdbg.h>
+#endif // _DEBUG
 #endif
 
 #include "irrlicht.h"
 
 #ifdef _IRR_COMPILE_WITH_WINDOWS_DEVICE_
-	#include "CIrrDeviceWin32.h"
+#include "CIrrDeviceWin32.h"
 #endif
 
 #ifdef _IRR_COMPILE_WITH_OSX_DEVICE_
-    #include "CIrrDevicePhone.h"
+#include "CIrrDevicePhone.h"
+#endif
+
+#ifdef _IRR_COMPILE_WITH_X11_DEVICE_
+#include "CIrrDeviceLinux.h"
 #endif
 
 #ifdef _IRR_COMPILE_WITH_CONSOLE_DEVICE_
-	#include "CIrrDeviceConsole.h"
+#include "CIrrDeviceConsole.h"
 #endif
 
 #ifdef _IRR_COMPILE_WITH_PHONE_DEVICE_
-	#include "CIrrDevicePhone.h"
+#include "CIrrDevicePhone.h"
 #endif
 
 #ifdef _IRR_COMPILE_WITH_SDL_DEVICE_
@@ -40,9 +44,9 @@ namespace irr
 {
 	//! stub for calling createDeviceEx
 	IRRLICHT_API IrrlichtDevice* IRRCALLCONV createDevice(video::E_DRIVER_TYPE driverType,
-			const core::dimension2d<u32>& windowSize,
-			u32 bits, bool fullscreen,
-			bool stencilbuffer, bool vsync, IEventReceiver* res)
+		const core::dimension2d<u32>& windowSize,
+		u32 bits, bool fullscreen,
+		bool stencilbuffer, bool vsync, IEventReceiver* res)
 	{
 		SIrrlichtCreationParameters p;
 		p.DriverType = driverType;
@@ -81,6 +85,16 @@ namespace irr
 			dev = new CIrrDeviceConsole(params);
 #endif
 
+#ifdef _IRR_COMPILE_WITH_X11_DEVICE_
+		if (params.DeviceType == EIDT_LINUX || (!dev && params.DeviceType == EIDT_BEST))
+			dev = new CIrrDeviceLinux(params);
+#endif
+
+#ifdef _IRR_COMPILE_WITH_CONSOLE_DEVICE_
+		if (params.DeviceType == EIDT_CONSOLE || (!dev && params.DeviceType == EIDT_BEST))
+			dev = new CIrrDeviceConsole(params);
+#endif
+
 #ifdef _IRR_COMPILE_WITH_PHONE_DEVICE_
 		if (params.DeviceType == EIDT_PHONE || (!dev && params.DeviceType == EIDT_BEST))
 			dev = new CIrrDevicePhone(params);
@@ -98,42 +112,42 @@ namespace irr
 
 		return dev;
 	}
-	
-namespace core
-{
-	const matrix4 IdentityMatrix(matrix4::EM4CONST_IDENTITY);
-	irr::core::stringc LOCALE_DECIMAL_POINTS(".");
-}
 
-namespace video
-{
-	SMaterial IdentityMaterial;
-}
-	
+	namespace core
+	{
+		const matrix4 IdentityMatrix(matrix4::EM4CONST_IDENTITY);
+		irr::core::stringc LOCALE_DECIMAL_POINTS(".");
+	}
+
+	namespace video
+	{
+		SMaterial IdentityMaterial;
+	}
+
 } // end namespace irr
 
 
 #if defined(_IRR_WINDOWS_API_) && !defined(_IRR_STATIC_LIB_)
 
-BOOL APIENTRY DllMain( HANDLE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved )
+BOOL APIENTRY DllMain(HANDLE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved)
 {
 	// _crtBreakAlloc = 139;
 
-    switch (ul_reason_for_call)
+	switch (ul_reason_for_call)
 	{
-		case DLL_PROCESS_ATTACH:
-			#if defined(_DEBUG) && !defined(__GNUWIN32__) && !defined(__BORLANDC__) && !defined (_IRR_XBOX_PLATFORM_)
-				_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
-			#endif
-			break;
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
-		case DLL_PROCESS_DETACH:
-			break;
-    }
-    return TRUE;
+	case DLL_PROCESS_ATTACH:
+#if defined(_DEBUG) && !defined(__GNUWIN32__) && !defined(__BORLANDC__) && !defined (_IRR_XBOX_PLATFORM_)
+		_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+#endif
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
 #endif // defined(_IRR_WINDOWS_)
