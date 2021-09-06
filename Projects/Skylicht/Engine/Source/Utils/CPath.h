7eff2c3b9a95e4e2d4a09d8add0dfd90f2cb020d
Pham Hong Duc <hongduc.pr@gmail.com>
2021-09-06 22:18:53 +0700

    #123 Implement browse file/folder on OpenSaveDialog

--- Projects/Skylicht/Engine/Source/Utils/CPath.h
+++ Projects/Skylicht/Engine/Source/Utils/CPath.h
@@ -39,6 +39,8 @@ namespace Skylicht
 
 		static std::string getFolderPath(const std::string& path);
 
+		static std::string getParentFolderPath(const std::string& path);
+
 		static std::string normalizePath(const std::string& path);
 
 		static std::string getRelativePath(const std::string& path, const std::string& folder);
