78022432df781895e861665d07ddfa8894d3362c
Pham Hong Duc <hongduc.pr@gmail.com>
2022-03-01 20:27:31 +0700

    #143 Implement shader sky & sun

---
+++ Assets/BuiltIn/Shader/SkySun/GLSL/LibSkySun.glsl
@@ -0,0 +1,26 @@
+vec3 GetSkyColor(
+	vec3 viewDir, 
+	vec3 sunDirection,
+	float intensity,
+	vec4 atmosphericColor,
+	vec4 sunColor,
+	vec4 glowColor1,
+	vec4 glowColor2,
+	float sunRadius)
+{
+	float y = 1.0 - (max(viewDir.y, 0.0) * 0.8 + 0.2) * 0.8;
+
+	// SKY
+	vec3 skyColor = vec3(pow(y, 2.0), y, 0.6 + y*0.4) * intensity;
+	
+	// SUN
+	float sunAmount = max(dot(sunDirection, viewDir), 0.0);
+	skyColor += atmosphericColor.rgb * sunAmount * sunAmount * atmosphericColor.a;
+	skyColor += sunColor.rgb * pow(sunAmount, sunRadius) * sunColor.a;
+	
+	// GLARE
+	skyColor += glowColor1.rgb * pow(sunAmount, 8.0) * glowColor1.a;
+	skyColor += glowColor2.rgb * pow(sunAmount, 3.0) * glowColor2.a;
+	
+	return skyColor;
+}
--- Assets/BuiltIn/Shader/SkySun/GLSL/SkySunFS.glsl
+++ Assets/BuiltIn/Shader/SkySun/GLSL/SkySunFS.glsl
@@ -18,16 +18,38 @@ vec3 linearRGB(vec3 color)
 {
 	return pow(color, vec3(invGamma));
 }
+vec3 GetSkyColor(
+	vec3 viewDir,
+	vec3 sunDirection,
+	float intensity,
+	vec4 atmosphericColor,
+	vec4 sunColor,
+	vec4 glowColor1,
+	vec4 glowColor2,
+	float sunRadius)
+{
+	float y = 1.0 - (max(viewDir.y, 0.0) * 0.8 + 0.2) * 0.8;
+	vec3 skyColor = vec3(pow(y, 2.0), y, 0.6 + y*0.4) * intensity;
+	float sunAmount = max(dot(sunDirection, viewDir), 0.0);
+	skyColor += atmosphericColor.rgb * sunAmount * sunAmount * atmosphericColor.a;
+	skyColor += sunColor.rgb * pow(sunAmount, sunRadius) * sunColor.a;
+	skyColor += glowColor1.rgb * pow(sunAmount, 8.0) * glowColor1.a;
+	skyColor += glowColor2.rgb * pow(sunAmount, 3.0) * glowColor2.a;
+	return skyColor;
+}
 void main(void)
 {
 	vec3 viewDir = normalize(varWorldPos.xyz - uCamPosition.xyz);
-	float y = 1.0 - (max(viewDir.y, 0.0) * 0.8 + 0.2) * 0.8;
-	vec3 skyColor = vec3(pow(y, 2.0), y, 0.6 + y*0.4) * 1.1;
-	float sunAmount = max(dot(uLightDirection.xyz, viewDir), 0.0);
-	skyColor = skyColor + vec3(1.0, 0.8, 0.7) * sunAmount * sunAmount * 0.1;
-	skyColor = skyColor + vec3(1.0, 0.6, 0.1) * pow(sunAmount, 800.0) * 0.5;
-	skyColor = skyColor + vec3(1.0,0.6,0.1)*pow(sunAmount, 8.0) * 0.4;
-	skyColor = skyColor + vec3(1.0,0.4,0.2)*pow(sunAmount, 3.0) * 0.2;
+	vec3 skyColor = GetSkyColor(
+		viewDir,
+		uLightDirection.xyz,
+		1.1,
+		vec4(1.0, 0.8, 0.7, 0.1),
+		vec4(1.0, 0.6, 0.1, 0.5),
+		vec4(1.0, 0.6, 0.1, 0.4),
+		vec4(1.0, 0.4, 0.2, 0.2),
+		800.0
+	);
 	vec3 groundColor = vec3(0.4, 0.4, 0.4);
 	vec3 result = mix(skyColor, sRGB(groundColor), pow(smoothstep(0.0,-0.025, viewDir.y), 0.2));
 	vec4 blend = varColor * uColor;
