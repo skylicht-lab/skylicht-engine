3dfa5c7c5ca78b28c4a648bbd6edbd0ca967c7d9
Pham Hong Duc <hongduc.pr@gmail.com>
2022-03-11 13:51:14 +0700

    #143 Fix build error, miss source

---
+++ Projects/Editor/Source/Editor/EntityData/WorldTransform/CWorldTransformDataEditor.h
@@ -0,0 +1,51 @@
+/*
+!@
+MIT License
+
+Copyright (c) 2022 Skylicht Technology CO., LTD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the Rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This file is part of the "Skylicht Engine".
+https://github.com/skylicht-lab/skylicht-engine
+!#
+*/
+
+#include "Editor/EntityData/CEntityDataEditor.h"
+#include "Activator/CEditorActivator.h"
+#include "Editor/Gizmos/Transform/CTransformGizmos.h"
+#include "Transform/CWorldTransformData.h"
+
+namespace Skylicht
+{
+	namespace Editor
+	{
+		class CWorldTransformDataEditor : public CEntityDataEditor
+		{
+		protected:
+			CWorldTransformData* m_worldTransform;
+
+		public:
+			CWorldTransformDataEditor();
+
+			virtual ~CWorldTransformDataEditor();
+
+			virtual void initGUI(IEntityData* entityData, CSpaceProperty* ui);
+
+			virtual void closeGUI();
+
+			virtual void update();
+		};
+	}
+}
