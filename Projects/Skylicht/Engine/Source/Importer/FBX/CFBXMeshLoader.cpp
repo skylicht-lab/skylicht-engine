4e51c37cddbb34f2b438efe0605163f662a27d0b
ducphamhong <hongduc.pr@gmail.com>
2022-06-27 17:40:48 +0700

    #136 Implement load fbx & fbx framework

--- Projects/Skylicht/Engine/Source/Importer/FBX/CFBXMeshLoader.cpp
+++ Projects/Skylicht/Engine/Source/Importer/FBX/CFBXMeshLoader.cpp
@@ -49,6 +49,60 @@ namespace Skylicht
 
 	bool CFBXMeshLoader::loadModel(const char* resource, CEntityPrefab* output, bool normalMap, bool flipNormalMap, bool texcoord2, bool batching)
 	{
+		IrrlichtDevice* device = getIrrlichtDevice();
+		io::IFileSystem* fs = device->getFileSystem();
+
+		io::IReadFile* file = fs->createAndOpenFile(resource);
+		if (file == NULL)
+		{
+			char log[64];
+			sprintf(log, "Can not load: %s\n", resource);
+			os::Printer::log(log);
+			return false;
+		}
+
+		const long filesize = file->getSize();
+		if (!filesize)
+			return 0;
+
+		c8* buf = new c8[filesize];
+		memset(buf, 0, filesize);
+		file->read((void*)buf, filesize);
+
+		ufbx_load_opts opts;
+		memset(&opts, 0, sizeof(ufbx_load_opts));
+
+		opts.load_external_files = true;
+		opts.allow_null_material = true;
+
+		// NOTE: We use this _only_ for computing the bounds of the scene!
+		// The viewer contains a proper implementation of skinning as well.
+		// You probably don't need this.
+		opts.evaluate_skinning = true;
+
+		opts.target_axes.right = UFBX_COORDINATE_AXIS_POSITIVE_X;
+		opts.target_axes.up = UFBX_COORDINATE_AXIS_POSITIVE_Y;
+		opts.target_axes.front = UFBX_COORDINATE_AXIS_POSITIVE_Z;
+
+		opts.target_unit_meters = 1.0f;
+
+		ufbx_error error;
+		ufbx_scene* scene = ufbx_load_memory(buf, filesize, &opts, &error);
+		if (!scene)
+		{
+			os::Printer::log("Failed to load scene");
+			delete buf;
+			file->drop();
+			return false;
+		}
+
+		// import scene
+
+		delete[]buf;
+		file->drop();
+
+		ufbx_free_scene(scene);
+
 		return false;
 	}
 }
--- Samples/Instancing/Source/SampleInstancing.cpp
+++ Samples/Instancing/Source/SampleInstancing.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
-#include "SkylichtEngine.h"
 #include "SampleInstancing.h"
 
-#include "GridPlane/CGridPlane.h"
+#include "Context/CContext.h"
+#include "ViewManager/CViewManager.h"
+
+#include "CViewInit.h"
 
 void installApplication(const std::vector<std::string>& argv)
 {
@@ -10,118 +12,65 @@ void installApplication(const std::vector<std::string>& argv)
 	getApplication()->registerAppEvent("SampleInstancing", app);
 }
 
-SampleInstancing::SampleInstancing() :
-	m_scene(NULL),
-	m_forwardRP(NULL)
+SampleInstancing::SampleInstancing()
 {
-
+	CContext::createGetInstance();
+	CViewManager::createGetInstance()->initViewLayer(1);
+	CLightmapper::createGetInstance();
 }
 
 SampleInstancing::~SampleInstancing()
 {
-	delete m_scene;
-	delete m_forwardRP;
+	CViewManager::releaseInstance();
+	CContext::releaseInstance();
+	CLightmapper::releaseInstance();
 }
 
 void SampleInstancing::onInitApp()
 {
-	// init application
-	CBaseApp* app = getApplication();
-
-	// load "BuiltIn.zip" to read files inside it
-	app->getFileSystem()->addFileArchive(app->getBuiltInPath("BuiltIn.zip"), false, false);
-
-	// load basic shader
-	CShaderManager* shaderMgr = CShaderManager::getInstance();
-	shaderMgr->initBasicShader();
-
-	// create a Scene
-	m_scene = new CScene();
-
-	// create a Zone in Scene
-	CZone* zone = m_scene->createZone();
-
-	// create 2D camera
-	CGameObject* guiCameraObject = zone->createEmptyObject();
-	m_guiCamera = guiCameraObject->addComponent<CCamera>();
-	m_guiCamera->setProjectionType(CCamera::OrthoUI);
-
-	// create 3D camera
-	CGameObject* camObj = zone->createEmptyObject();
-	camObj->addComponent<CCamera>();
-	camObj->addComponent<CEditorCamera>()->setMoveSpeed(2.0f);
-
-	m_camera = camObj->getComponent<CCamera>();
-	m_camera->setPosition(core::vector3df(0.0f, 1.5f, 4.0f));
-	m_camera->lookAt(core::vector3df(0.0f, 0.0f, 0.0f), core::vector3df(0.0f, 1.0f, 0.0f));
-
-	// 3d grid
-	CGameObject* grid = zone->createEmptyObject();
-	grid->addComponent<CGridPlane>();
-
-	// lighting
-	CGameObject* lightObj = zone->createEmptyObject();
-	CDirectionalLight* directionalLight = lightObj->addComponent<CDirectionalLight>();
-	SColor c(255, 255, 244, 214);
-	directionalLight->setColor(SColorf(c));
-
-	CTransformEuler* lightTransform = lightObj->getTransformEuler();
-	lightTransform->setPosition(core::vector3df(2.0f, 2.0f, 2.0f));
-
-	core::vector3df direction = core::vector3df(0.0f, -1.5f, 2.0f);
-	lightTransform->setOrientation(direction, CTransform::s_oy);
-
-	// rendering pipe line
-	u32 w = app->getWidth();
-	u32 h = app->getHeight();
-
-	m_forwardRP = new CForwardRP();
-	m_forwardRP->initRender(w, h);
+	CViewManager::getInstance()->getLayer(0)->pushView<CViewInit>();
 }
 
 void SampleInstancing::onUpdate()
 {
-	// update application
-	m_scene->update();
+	CViewManager::getInstance()->update();
 }
 
 void SampleInstancing::onRender()
 {
-	// render 3d scene
-	m_forwardRP->render(NULL, m_camera, m_scene->getEntityManager(), core::recti());
-
-	// render text in gui camera
-	CGraphics2D::getInstance()->render(m_guiCamera);
+	CViewManager::getInstance()->render();
 }
 
 void SampleInstancing::onPostRender()
 {
 	// post render application
+	CViewManager::getInstance()->postRender();
 }
 
 bool SampleInstancing::onBack()
 {
 	// on back key press
 	// return TRUE will run default by OS (Mobile)
-	// return FALSE will cancel BACK FUNCTION by OS (Mobile)
-	return true;
+	// return FALSE will cancel BACK FUNCTION by OS (Mobile)	
+	return CViewManager::getInstance()->onBack();
 }
 
 void SampleInstancing::onResize(int w, int h)
 {
-	// on window size changed
-	if (m_forwardRP != NULL)
-		m_forwardRP->resize(w, h);
+	if (CContext::getInstance() != NULL)
+		CContext::getInstance()->resize(w, h);
 }
 
 void SampleInstancing::onResume()
 {
 	// resume application
+	CViewManager::getInstance()->onResume();
 }
 
 void SampleInstancing::onPause()
 {
 	// pause application
+	CViewManager::getInstance()->onPause();
 }
 
 void SampleInstancing::onQuitApp()
