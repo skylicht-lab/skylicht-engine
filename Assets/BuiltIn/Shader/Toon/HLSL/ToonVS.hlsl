54eef20862549d17f6f1c06c33e009eb0fdc506a
ducphamhong <hongduc.pr@gmail.com>
2022-07-07 17:45:36 +0700

    #136 Add toon shader & empty shader

---
+++ Assets/BuiltIn/Shader/Toon/HLSL/ToonVS.hlsl
@@ -0,0 +1,29 @@
+struct VS_INPUT
+{
+	float4 pos: POSITION;
+	float3 norm: NORMAL;
+	float4 color: COLOR;
+	float2 tex0: TEXCOORD0;
+};
+
+struct VS_OUTPUT
+{
+	float4 pos : SV_POSITION;
+	float2 tex0 : TEXCOORD0;
+};
+
+cbuffer cbPerObject
+{
+	float4x4 uMvpMatrix;
+	float4 uUVScale;
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output;
+
+	output.pos = mul(input.pos, uMvpMatrix);
+	output.tex0 = input.tex0 * uUVScale.xy + uUVScale.zw;
+
+	return output;
+}
