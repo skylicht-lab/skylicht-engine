cc7f86aa5762e4a728c9da6eaa1d1539773e648f
ducphamhong <hongduc.pr@gmail.com>
2022-07-11 16:26:32 +0700

    #136 Implement toon shader

---
+++ Assets/BuiltIn/Shader/Toon/GLSL/SkinToonVS.glsl
@@ -0,0 +1,49 @@
+in vec4 inPosition;
+in vec3 inNormal;
+in vec4 inColor;
+in vec2 inTexCoord0;
+in vec4 inBlendIndex;
+in vec4 inBlendWeight;
+
+uniform mat4 uMvpMatrix;
+uniform mat4 uWorldMatrix;
+uniform vec4 uCameraPosition;
+uniform vec4 uUVScale;
+uniform mat4 uBoneMatrix[64];
+
+out vec2 vTexCoord0;
+out vec3 vWorldNormal;
+out vec3 vWorldViewDir;
+
+void main(void)
+{
+	mat4 skinMatrix = mat4(0.0);
+	vec4 skinPosition = vec4(0.0);
+	vec3 skinNormal = vec3(0.0);
+
+	int index = int(inBlendIndex[0]);
+	skinMatrix = inBlendWeight[0] * uBoneMatrix[index];
+
+	index = int(inBlendIndex[1]);
+	skinMatrix += inBlendWeight[1] * uBoneMatrix[index];
+
+	index = int(inBlendIndex[2]);
+	skinMatrix += inBlendWeight[2] * uBoneMatrix[index];
+
+	index = int(inBlendIndex[3]);
+	skinMatrix += inBlendWeight[3] * uBoneMatrix[index];
+
+	skinPosition = skinMatrix * inPosition;
+	skinNormal = (skinMatrix * vec4(inNormal, 0.0)).xyz;
+	
+	vTexCoord0 = inTexCoord0 * uUVScale.xy + uUVScale.zw;
+	
+	vec4 worldPos = uWorldMatrix * skinPosition;
+	vec4 worldViewDir = normalize(uCameraPosition - worldPos);
+	vec4 worldNormal = uWorldMatrix * vec4(skinNormal, 0.0);
+	
+	vWorldNormal = normalize(worldNormal.xyz);
+	vWorldViewDir = worldViewDir.xyz;
+	
+	gl_Position = uMvpMatrix * skinPosition;
+}
---
+++ Assets/BuiltIn/Shader/Toon/HLSL/SkinToonVS.hlsl
@@ -0,0 +1,67 @@
+struct VS_INPUT
+{
+	float4 pos: POSITION;
+	float3 norm: NORMAL;
+	float4 color: COLOR;
+	float2 tex0: TEXCOORD0;
+	float4 blendIndex : BLENDINDICES;
+	float4 blendWeight : BLENDWEIGHT;
+};
+
+struct VS_OUTPUT
+{
+	float4 pos : SV_POSITION;
+	float2 tex0 : TEXCOORD0;
+	float3 worldNormal: WORLDNORMAL;
+	float3 worldViewDir: WORLDVIEWDIR;
+};
+
+cbuffer cbPerObject
+{
+	float4x4 uMvpMatrix;
+	float4x4 uWorldMatrix;
+	float4 uCameraPosition;
+	float4 uUVScale;
+	float4x4 uBoneMatrix[64];
+};
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output;
+
+	float4x4 skinMatrix;
+	float4 skinPosition;
+	float4 skinNormal;
+
+	// bone 0
+	int index = int(input.blendIndex[0]);
+	skinMatrix = input.blendWeight[0] * uBoneMatrix[index];
+
+	// bone 1
+	index = int(input.blendIndex[1]);
+	skinMatrix += input.blendWeight[1] * uBoneMatrix[index];
+
+	// bone 2
+	index = int(input.blendIndex[2]);
+	skinMatrix += input.blendWeight[2] * uBoneMatrix[index];
+
+	// bone 3
+	index = int(input.blendIndex[3]);
+	skinMatrix += input.blendWeight[3] * uBoneMatrix[index];
+
+	// skin result
+	skinPosition = mul(input.pos, skinMatrix);
+	skinNormal 	= mul(float4(input.norm, 0.0), skinMatrix);
+
+	float4 worldPos = mul(skinPosition, uWorldMatrix);
+
+	float4 worldNormal = mul(float4(skinNormal.xyz, 0.0), uWorldMatrix);
+	float4 worldViewDir = normalize(uCameraPosition - worldPos);
+
+	output.pos = mul(skinPosition, uMvpMatrix);
+	output.tex0 = input.tex0 * uUVScale.xy + uUVScale.zw;
+	output.worldNormal = normalize(worldNormal.xyz);
+	output.worldViewDir = worldViewDir.xyz;
+
+	return output;
+}
