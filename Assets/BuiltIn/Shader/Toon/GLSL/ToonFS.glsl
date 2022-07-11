2cea1f56b52a55eb6819467fd034abd5e6e59be7
ducphamhong <hongduc.pr@gmail.com>
2022-07-11 16:26:32 +0700

    #136 Implement toon shader

--- Assets/BuiltIn/Shader/Toon/GLSL/ToonFS.glsl
+++ Assets/BuiltIn/Shader/Toon/GLSL/ToonFS.glsl
@@ -1,8 +1,14 @@
 precision mediump float;
 
 uniform sampler2D uTexDiffuse;
-uniform vec4 uColor;
+uniform sampler2D uTexRamp;
+
 uniform vec4 uLightDirection;
+uniform vec4 uLightColor;
+uniform vec4 uColor;
+uniform vec4 uShadowColor;
+uniform vec2 uWrapFactor;
+uniform vec3 uSpecular;
 
 in vec2 vTexCoord0;
 in vec3 vWorldNormal;
@@ -14,7 +20,20 @@ void main(void)
 {
 	vec4 diffuseMap = texture(uTexDiffuse, vTexCoord0.xy);
 	
-	float NdotL = max(dot(vWorldNormal, uLightDirection.xyz), 0.0);
+	float NdotL = max((dot(vWorldNormal, uLightDirection.xyz) + uWrapFactor.x) / (1.0 + uWrapFactor.x), 0.0);
 	
-	FragColor = diffuseMap * uColor * NdotL;
+	vec3 rampMap = texture(uTexRamp, vec2(NdotL, NdotL)).rgb;
+	
+	// Shadows intensity through alpha
+	vec3 color = mix(uColor.rgb, uShadowColor.rgb, uColor.a);
+	vec3 ramp = mix(color, uColor.rgb, rampMap);
+	
+	// Specular
+	vec3 h = normalize(uLightDirection.xyz + vWorldViewDir);
+	float NdotH = max(0, dot(vWorldNormal, h));
+	
+	float spec = pow(NdotH, uSpecular.x*128.0) * uSpecular.y;
+	spec = smoothstep(0.5-uSpecular.z*0.5, 0.5+uSpecular.z*0.5, spec);
+	
+	FragColor = diffuseMap * uLightColor * vec4(ramp, 1.0) + uLightColor * spec;
 }
