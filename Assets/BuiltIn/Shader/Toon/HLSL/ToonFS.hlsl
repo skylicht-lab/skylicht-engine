5a4cd0faa1d6887e865e260da10e4c5f67b6cc49
ducphamhong <hongduc.pr@gmail.com>
2022-07-11 16:26:32 +0700

    #136 Implement toon shader

--- Assets/BuiltIn/Shader/Toon/HLSL/ToonFS.hlsl
+++ Assets/BuiltIn/Shader/Toon/HLSL/ToonFS.hlsl
@@ -1,6 +1,9 @@
 Texture2D uTexDiffuse : register(t0);
 SamplerState uTexDiffuseSampler : register(s0);
 
+Texture2D uTexRamp : register(t1);
+SamplerState uTexRampSampler : register(s1);
+
 struct PS_INPUT
 {
 	float4 pos : SV_POSITION;
@@ -11,15 +14,33 @@ struct PS_INPUT
 
 cbuffer cbPerFrame
 {
-	float4 uColor;
 	float4 uLightDirection;
+	float4 uLightColor;
+	float4 uColor;
+	float4 uShadowColor;
+	float2 uWrapFactor;
+	float3 uSpecular;
 };
 
 float4 main(PS_INPUT input) : SV_TARGET
 {	
 	float4 diffuseMap = uTexDiffuse.Sample(uTexDiffuseSampler, input.tex0);
 	
-	float NdotL = max(dot(input.worldNormal, uLightDirection.xyz), 0.0);
+	float NdotL = max((dot(input.worldNormal, uLightDirection.xyz) + uWrapFactor.x) / (1.0 + uWrapFactor.x), 0.0);
 	
-	return diffuseMap * uColor * NdotL;
+	float3 rampMap = uTexRamp.Sample(uTexRampSampler, float2(NdotL, NdotL)).rgb;
+	
+	// Shadows intensity through alpha
+	float3 color = lerp(uColor.rgb, uShadowColor.rgb, uColor.a);
+	
+	float3 ramp = lerp(color, uColor.rgb, rampMap);
+	
+	// Specular
+	float3 h = normalize(uLightDirection.xyz + input.worldViewDir);
+	float NdotH = max(0, dot(input.worldNormal, h));
+	
+	float spec = pow(NdotH, uSpecular.x*128.0) * uSpecular.y;
+	spec = smoothstep(0.5-uSpecular.z*0.5, 0.5+uSpecular.z*0.5, spec);
+	
+	return diffuseMap * uLightColor * float4(ramp, 1.0) + uLightColor * spec;
 }
