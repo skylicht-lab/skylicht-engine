84c3cc52f8284e7554aa1b96ad1a941270baf4c3
ducphamhong <hongduc.pr@gmail.com>
2022-09-13 16:42:21 +0700

    #157 Add render data

--- Projects/Skylicht/Engine/Source/Graphics2D/EntityData/CGUIRenderData.h
+++ Projects/Skylicht/Engine/Source/Graphics2D/EntityData/CGUIRenderData.h
@@ -2,7 +2,7 @@
 !@
 MIT License
 
-Copyright (c) 2019 Skylicht Technology CO., LTD
+Copyright (c) 2022 Skylicht Technology CO., LTD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
@@ -22,52 +22,31 @@ https://github.com/skylicht-lab/skylicht-engine
 !#
 */
 
-#include "pch.h"
-#include "CGUIImage.h"
-#include "Graphics2D/CGraphics2D.h"
+#pragma once
+
+#include "Entity/IEntityData.h"
+#include "Material/CMaterial.h"
 
 namespace Skylicht
 {
-	CGUIImage::CGUIImage(CCanvas* canvas, CGUIElement* parent) :
-		CGUIElement(canvas, parent),
-		m_image(NULL)
+	class CGUIRenderData : public IEntityData
 	{
+		friend class CGUILayoutSystem;
 
-	}
+	public:
+		DECLARE_DATA_TYPE_INDEX;
 
-	CGUIImage::CGUIImage(CCanvas* canvas, CGUIElement* parent, const core::rectf& rect) :
-		CGUIElement(canvas, parent, rect),
-		m_image(NULL)
-	{
+		SColor Color;
 
-	}
+		int ShaderID;
 
-	CGUIImage::~CGUIImage()
-	{
+		CMaterial* Material;
 
-	}
+	public:
+		CGUIRenderData();
 
-	void CGUIImage::render(CCamera* camera)
-	{
-		if (m_image != NULL)
-		{
-			CGraphics2D::getInstance()->addImageBatch(
-				m_image,
-				getRect(),
-				m_sourceRect,
-				getColor(),
-				m_transform->World,
-				getShaderID(),
-				getMaterial(),
-				m_pivot.X, m_pivot.Y);
-		}
-	}
+		virtual ~CGUIRenderData();
 
-	void CGUIImage::setImage(ITexture* texture)
-	{
-		m_image = texture;
-
-		if (m_image)
-			setSourceRect(0, 0, (float)m_image->getSize().Width, (float)m_image->getSize().Height);
-	}
+		DECLARE_GETTYPENAME(CGUIRenderData);
+	};
 }
