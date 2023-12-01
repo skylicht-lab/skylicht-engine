f7a98f9f8fd23d4aa520d10876d32ba6f842b522
ducphamhong <hongduc.pr@gmail.com>
2023-12-01 17:56:47 +0700

    #158 Add custom size sprite

---
+++ Projects/Skylicht/Engine/Source/Graphics2D/GUI/CGUICustomSizeSprite.h
@@ -0,0 +1,80 @@
+/*
+!@
+MIT License
+
+Copyright (c) 2019 Skylicht Technology CO., LTD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This file is part of the "Skylicht Engine".
+https://github.com/skylicht-lab/skylicht-engine
+!#
+*/
+
+#pragma once
+
+#include "CGUIElement.h"
+#include "Graphics2D/SpriteFrame/CSpriteFrame.h"
+
+namespace Skylicht
+{
+	class CGUICustomSizeSprite : public CGUIElement
+	{
+		friend class CCanvas;
+	public:
+		enum AnchorType
+		{
+			Normal,
+			AnchorAll,
+			AnchorLeftRight,
+			AnchorTopBottom
+		};
+
+	protected:
+		SFrame* m_frame;
+
+		std::string m_guid;
+		std::string m_frameName;
+		std::string m_sprite;
+		std::string m_spriteId;
+
+		AnchorType m_anchorType;
+
+		float m_anchorLeft;
+		float m_anchorRight;
+		float m_anchorTop;
+		float m_anchorBottom;
+
+	protected:
+		CGUICustomSizeSprite(CCanvas* canvas, CGUIElement* parent, SFrame* frame);
+		CGUICustomSizeSprite(CCanvas* canvas, CGUIElement* parent, const core::rectf& rect, SFrame* frame);
+
+	public:
+		virtual ~CGUICustomSizeSprite();
+
+		virtual void update(CCamera* camera);
+
+		virtual void render(CCamera* camera);
+
+		void setFrame(SFrame* frame);
+
+		void setAnchor(AnchorType type, float left, float right, float top, float bottom);
+
+		virtual CObjectSerializable* createSerializable();
+
+		virtual void loadSerializable(CObjectSerializable* object);
+
+		DECLARE_GETTYPENAME(CGUICustomSizeSprite);
+	};
+}
