15e30b1a9421469c6b804e3a4330b1ad29b2faa4
Pham Hong Duc <>
2024-02-02 18:09:37 +0700

    Update structure of Angle project

--- CMake/CMakeVisualStudioSourceGroup.cmake
+++ CMake/CMakeVisualStudioSourceGroup.cmake
@@ -5,15 +5,40 @@ function(setup_project_group project_source current_dir)
 		get_filename_component(source_path ${source} PATH)
 		
 		# get source relative path
-		string(REPLACE "${current_dir}/./" "" source_relative ${source_path})
-		
+		string(REPLACE "${current_dir}/./" "" source_relative ${source_path})		
+
 		if(MSVC OR XCODE)
 			# get group name
 			string(REPLACE "/" "\\" group_name ${source_relative})
 		else()
 			set(group_name ${source_relative})
 		endif()
+
+		if("${source_relative}" STREQUAL "${source_path}")
+			# no need group because source is in $current_dir
+		else()
+			# setup project group
+			source_group("${group_name}" FILES "${source}")
+		endif()
+	endforeach()
+endfunction()
+
+function(setup_project_group_by_folder_relative project_source current_dir)
+	foreach(source IN LISTS project_source)
+		# get source path
+		get_filename_component(source_path ${source} PATH)
 		
+		# get source relative path
+		string(REPLACE "${current_dir}" "" source_relative ${source_path})		
+
+		if(MSVC OR XCODE)
+			# get group name
+			string(REPLACE "/" "\\" group_name ${source_relative})
+			set(group_name "Source Files\\${group_name}")
+		else()
+			set(group_name "Source Files\\${source_relative}")
+		endif()
+
 		if("${source_relative}" STREQUAL "${source_path}")
 			# no need group because source is in $current_dir
 		else()
