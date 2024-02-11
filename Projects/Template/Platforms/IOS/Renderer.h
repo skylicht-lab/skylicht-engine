8e8baa58a2a7efbe4d3e5efc9db6cea72e4307d1
Pham Hong Duc <>
2024-02-11 16:23:50 +0700

    Add base IOS Application (not yet init Angle)

---
+++ Projects/Template/Platforms/IOS/Renderer.h
@@ -0,0 +1,17 @@
+//
+//  Renderer.h
+//  DefaultMetal
+//
+//  Created by Pham Hong Duc on 10/02/2024.
+//
+
+#import <MetalKit/MetalKit.h>
+
+// Our platform independent renderer class.   Implements the MTKViewDelegate protocol which
+//   allows it to accept per-frame update and drawable resize callbacks.
+@interface Renderer : NSObject <MTKViewDelegate>
+
+-(nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view;
+
+@end
+
