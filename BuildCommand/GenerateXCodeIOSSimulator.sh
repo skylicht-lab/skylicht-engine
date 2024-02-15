fb9e6cf829e0c6b40637e9b77ad10b7db0fdcb47
ducphamhong <hongduc.pr@gmail.com>
2024-02-15 11:51:18 +0700

    Update IOS CI

--- BuildCommand/GenerateXCodeIOSSimulator.sh
+++ BuildCommand/GenerateXCodeIOSSimulator.sh
@@ -2,4 +2,4 @@ cd ..
 cd Assets
 python BuildAssetBundles.py
 cd ..
-cmake -S . -B ./PrjIOS -G Xcode -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DIOS_PLATFORM=SIMULATORARM64
+cmake -S . -B ./PrjIOS -G Xcode -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DPLATFORM=SIMULATORARM64
