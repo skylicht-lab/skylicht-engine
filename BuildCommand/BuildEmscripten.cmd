079dc165f81b37a9cb28e0d9090e205ed74c074e
ducphamhong <hongduc.pr@gmail.com>
2024-02-16 11:13:38 +0700

    Update build command & fix build error on TestApp/CMakeLists.txt

--- BuildCommand/BuildEmscripten.cmd
+++ BuildCommand/BuildEmscripten.cmd
@@ -1,8 +1,8 @@
 cd..
-cmake -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\upstream\emscripten\cmake\Modules\Platform\Emscripten.cmake  -S . -B ./PrjEmscripten -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND
 cd Assets
 python BuildAssetBundles.py
-cd ..
+cd..
 mkdir Bin\BuiltIn
 copy Bin\BuiltIn.zip Bin\BuiltIn
+cmake -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\upstream\emscripten\cmake\Modules\Platform\Emscripten.cmake  -S . -B ./PrjEmscripten -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND
 C:\MinGW\bin\mingw32-make.exe -C PrjEmscripten
