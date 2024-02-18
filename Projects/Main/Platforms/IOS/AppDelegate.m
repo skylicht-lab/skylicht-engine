7ab4d7ffe3975a400e6339665fdcbe00fb0aca60
Pham Hong Duc <>
2024-02-18 10:48:06 +0700

    Update project iOS (touch, save folder...) Fix audio engine on iOS & MacOS

--- Projects/Main/Platforms/IOS/AppDelegate.m
+++ Projects/Main/Platforms/IOS/AppDelegate.m
@@ -7,6 +7,9 @@
 
 #import "AppDelegate.h"
 
+// void notificationPause();
+// void notificationResume();
+
 @interface AppDelegate ()
 
 @end
@@ -23,6 +26,9 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    NSLog(@"applicationWillResignActive");
+    // notificationPause();
 }
 
 
@@ -38,6 +44,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    NSLog(@"applicationWillResignActive");
+    // notificationResume();
 }
 
 
