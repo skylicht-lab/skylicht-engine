9c9b3aaeb24317dd00249c52b4146a39dd61af18
Pham Hong Duc <>
2024-02-25 10:26:56 +0700

    Rebuild Angle library for iOS Apple device (test run ok on device)

--- BuildCommand/GenerateXCodeIOS.sh
+++ BuildCommand/GenerateXCodeIOS.sh
@@ -2,4 +2,4 @@ cd ..
 cd Assets
 python3 BuildAssetBundles.py
 cd ..
-cmake -S . -B ./PrjIOS -G Xcode -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DPLATFORM=OS
+cmake -S . -B ./PrjIOS -G Xcode -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=./ios.cmake -DPLATFORM=OS64
