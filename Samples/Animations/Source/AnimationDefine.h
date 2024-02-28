37f4aecaf8ce594d3f71fd2afb40bd6063fa2149
ducphamhong <hongduc.pr@gmail.com>
2024-02-28 12:30:44 +0700

    Add animation blending for sample animation

---
+++ Samples/Animations/Source/AnimationDefine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+enum class EAnimationId
+{
+	Idle = 0,
+	WalkBack,
+	WalkForward,
+	WalkLeft,
+	WalkRight,
+	Walk,
+	RunBack,
+	RunForward,
+	RunLeft,
+	RunRight,
+	Run,
+	Movement,
+	Result,
+};
