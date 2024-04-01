7f9d2196f648ca4af3c6fe62bc625b307fc43f6a
ducphamhong <hongduc.pr@gmail.com>
2024-04-01 14:27:52 +0700

    Update sample projects

--- Samples/SkinnedMeshInstancing/Source/SampleSkinnedMeshInstancing.cpp
+++ Samples/SkinnedMeshInstancing/Source/SampleSkinnedMeshInstancing.cpp
@@ -1,8 +1,10 @@
 #include "pch.h"
-#include "SkylichtEngine.h"
 #include "SampleSkinnedMeshInstancing.h"
 
-#include "GridPlane/CGridPlane.h"
+#include "Context/CContext.h"
+#include "ViewManager/CViewManager.h"
+
+#include "CViewInit.h"
 
 void installApplication(const std::vector<std::string>& argv)
 {
@@ -10,136 +12,65 @@ void installApplication(const std::vector<std::string>& argv)
 	getApplication()->registerAppEvent("SampleSkinnedMeshInstancing", app);
 }
 
-SampleSkinnedMeshInstancing::SampleSkinnedMeshInstancing() :
-	m_scene(NULL),
-	m_forwardRP(NULL),
-	m_largeFont(NULL)
+SampleSkinnedMeshInstancing::SampleSkinnedMeshInstancing()
 {
-
+	CContext::createGetInstance();
+	CViewManager::createGetInstance()->initViewLayer(1);
+	CLightmapper::createGetInstance();
 }
 
 SampleSkinnedMeshInstancing::~SampleSkinnedMeshInstancing()
 {
-	delete m_scene;
-	delete m_largeFont;
-	delete m_forwardRP;
+	CViewManager::releaseInstance();
+	CContext::releaseInstance();
+	CLightmapper::releaseInstance();
 }
 
 void SampleSkinnedMeshInstancing::onInitApp()
 {
-	// init application
-	CBaseApp* app = getApplication();
-
-	// load "BuiltIn.zip" to read files inside it
-	app->getFileSystem()->addFileArchive(app->getBuiltInPath("BuiltIn.zip"), false, false);
-
-	// init segoeuil.ttf inside BuiltIn.zip
-	CGlyphFreetype *freetypeFont = CGlyphFreetype::getInstance();
-	freetypeFont->initFont("Segoe UI Light", "BuiltIn/Fonts/segoeui/segoeuil.ttf");
-
-	// load basic shader
-	CShaderManager *shaderMgr = CShaderManager::getInstance();
-	shaderMgr->initBasicShader();
-
-	// create a Scene
-	m_scene = new CScene();
-
-	// create a Zone in Scene
-	CZone *zone = m_scene->createZone();
-
-	// create 2D camera
-	CGameObject *guiCameraObject = zone->createEmptyObject();
-	m_guiCamera = guiCameraObject->addComponent<CCamera>();
-	m_guiCamera->setProjectionType(CCamera::OrthoUI);
-
-	// create 3D camera
-	CGameObject *camObj = zone->createEmptyObject();
-	camObj->addComponent<CCamera>();
-	camObj->addComponent<CEditorCamera>()->setMoveSpeed(2.0f);
-
-	m_camera = camObj->getComponent<CCamera>();
-	m_camera->setPosition(core::vector3df(0.0f, 1.5f, 4.0f));
-	m_camera->lookAt(core::vector3df(0.0f, 0.0f, 0.0f), core::vector3df(0.0f, 1.0f, 0.0f));
-
-	// 3d grid
-	CGameObject *grid = zone->createEmptyObject();
-	grid->addComponent<CGridPlane>();
-
-	// lighting
-	CGameObject *lightObj = zone->createEmptyObject();
-	CDirectionalLight *directionalLight = lightObj->addComponent<CDirectionalLight>();
-	SColor c(255, 255, 244, 214);
-	directionalLight->setColor(SColorf(c));
-
-	CTransformEuler *lightTransform = lightObj->getTransformEuler();
-	lightTransform->setPosition(core::vector3df(2.0f, 2.0f, 2.0f));
-
-	core::vector3df direction = core::vector3df(0.0f, -1.5f, 2.0f);
-	lightTransform->setOrientation(direction, Transform::Oy);
-
-	m_largeFont = new CGlyphFont();
-	m_largeFont->setFont("Segoe UI Light", 50);
-
-	// create 2D Canvas
-	CGameObject *canvasObject = zone->createEmptyObject();
-	CCanvas *canvas = canvasObject->addComponent<CCanvas>();
-
-	// create UI Text in Canvas
-	CGUIText *textLarge = canvas->createText(m_largeFont);
-	textLarge->setText("SampleSkinnedMeshInstancing");
-	textLarge->setTextAlign(EGUIHorizontalAlign::Left, EGUIVerticalAlign::Top);
-
-	// rendering pipe line
-	u32 w = app->getWidth();
-	u32 h = app->getHeight();
-
-	m_forwardRP = new CForwardRP();
-	m_forwardRP->initRender(w, h);
+	CViewManager::getInstance()->getLayer(0)->pushView<CViewInit>();
 }
 
 void SampleSkinnedMeshInstancing::onUpdate()
 {
-	// update application
-	m_scene->update();
+	CViewManager::getInstance()->update();
 }
 
 void SampleSkinnedMeshInstancing::onRender()
 {
-	// render 3d scene
-	m_forwardRP->render(NULL, m_camera, m_scene->getEntityManager(), core::recti());
-
-	// render text in gui camera
-	CGraphics2D::getInstance()->render(m_guiCamera);
+	CViewManager::getInstance()->render();
 }
 
 void SampleSkinnedMeshInstancing::onPostRender()
 {
 	// post render application
+	CViewManager::getInstance()->postRender();
 }
 
 bool SampleSkinnedMeshInstancing::onBack()
 {
 	// on back key press
 	// return TRUE will run default by OS (Mobile)
-	// return FALSE will cancel BACK FUNCTION by OS (Mobile)
-	return true;
+	// return FALSE will cancel BACK FUNCTION by OS (Mobile)	
+	return CViewManager::getInstance()->onBack();
 }
 
 void SampleSkinnedMeshInstancing::onResize(int w, int h)
 {
-	// on window size changed
-	if (m_forwardRP != NULL)
-		m_forwardRP->resize(w, h);
+	if (CContext::getInstance() != NULL)
+		CContext::getInstance()->resize(w, h);
 }
 
 void SampleSkinnedMeshInstancing::onResume()
 {
 	// resume application
+	CViewManager::getInstance()->onResume();
 }
 
 void SampleSkinnedMeshInstancing::onPause()
 {
 	// pause application
+	CViewManager::getInstance()->onPause();
 }
 
 void SampleSkinnedMeshInstancing::onQuitApp()
