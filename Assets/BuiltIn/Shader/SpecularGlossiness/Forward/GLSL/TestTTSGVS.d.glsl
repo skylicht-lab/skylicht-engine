c502368d0f6557c347ff8a3ef93524639f09e20b
ducphamhong <hongduc.pr@gmail.com>
2024-04-03 09:30:57 +0700

    Fix test shader on GLES3

--- Assets/BuiltIn/Shader/SpecularGlossiness/Forward/GLSL/TestTTSGVS.d.glsl
+++ Assets/BuiltIn/Shader/SpecularGlossiness/Forward/GLSL/TestTTSGVS.d.glsl
@@ -39,17 +39,17 @@ mat4 getTransformFromTexture(vec2 p)
 	float nextPixelY = 1.0 / uTransformTextureSize.y;
 	
 	vec2 uv = vec2(
-		p.x * nextPixelX * 4 + centerX, 
+		p.x * nextPixelX * 4.0 + centerX, 
 		p.y * nextPixelY + centerY
 	);
 	
-	vec4 c1 = textureLod(uTransformTexture, uv, 0);
+	vec4 c1 = textureLod(uTransformTexture, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	vec4 c2 = textureLod(uTransformTexture, uv, 0);
+	vec4 c2 = textureLod(uTransformTexture, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	vec4 c3 = textureLod(uTransformTexture, uv, 0);
+	vec4 c3 = textureLod(uTransformTexture, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	vec4 c4 = textureLod(uTransformTexture, uv, 0);
+	vec4 c4 = textureLod(uTransformTexture, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
 	
 	return mat4(c1, c2, c3, c4);
--- Assets/BuiltIn/Shader/SpecularGlossiness/Forward/HLSL/TestTTSGVS.hlsl
+++ Assets/BuiltIn/Shader/SpecularGlossiness/Forward/HLSL/TestTTSGVS.hlsl
@@ -35,16 +35,16 @@ float4x4 getTransformFromTexture(float2 p)
 	float nextPixelX = 1.0 / uTransformTextureSize.x;
 	float nextPixelY = 1.0 / uTransformTextureSize.y;
 	float2 uv = float2(
-		p.x * nextPixelX * 4 + centerX,
+		p.x * nextPixelX * 4.0 + centerX,
 		p.y * nextPixelY + centerY
 	);
-	float4 c1 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0);
+	float4 c1 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	float4 c2 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0);
+	float4 c2 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	float4 c3 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0);
+	float4 c3 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
-	float4 c4 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0);
+	float4 c4 = uTransformTexture.SampleLevel(uTransformTextureSampler, uv, 0.0);
 	uv.x = uv.x + nextPixelX;
 	return float4x4(c1, c2, c3, c4);
 }
