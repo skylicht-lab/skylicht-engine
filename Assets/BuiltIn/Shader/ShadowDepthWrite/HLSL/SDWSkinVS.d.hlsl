75397ac6f48d03aed42cf3bb30e30e9f6acba49f
ducphamhong <hongduc.pr@gmail.com>
2024-04-17 17:49:43 +0700

    Add write shadow depth shader for Skinned Instancing

---
+++ Assets/BuiltIn/Shader/ShadowDepthWrite/HLSL/SDWSkinVS.d.hlsl
@@ -0,0 +1,73 @@
+Texture2D uTransformTexture : register(t0);
+SamplerState uTransformTextureSampler : register(s);
+
+struct VS_INPUT
+{
+	float4 pos: POSITION;
+	float3 norm: NORMAL;
+	float4 color: COLOR;
+	float2 tex0: TEXCOORD0;
+	float3 tangent: TANGENT;
+	float3 binormal: BINORMAL;
+	float2 data: DATA;
+	float4 blendIndex : BLENDINDICES;
+	float4 blendWeight : BLENDWEIGHT;
+	
+	float4 uBoneLocation: TEXCOORD1;
+	float4 uColor: TEXCOORD2;
+	float4 uSpecGloss: TEXCOORD3;
+	
+	float4x4 worldMatrix: TEXCOORD4;
+};
+
+struct VS_OUTPUT
+{
+	float4 pos : SV_POSITION;
+	float4 vPos: POSITION;
+};
+
+cbuffer cbPerObject
+{
+	float4x4 uVPMatrix;
+	float4 uAnimation;
+	float2 uTransformTextureSize;
+};
+
+#include "../../TransformTexture/HLSL/LibTransformTexture.hlsl"
+
+VS_OUTPUT main(VS_INPUT input)
+{
+	VS_OUTPUT output;
+
+	float4x4 uWorldMatrix = transpose(input.worldMatrix);
+	float4x4 uMvpMatrix = mul(uWorldMatrix, uVPMatrix);
+
+	float4x4 skinMatrix;
+	float4 skinPosition;
+
+	float2 boneLocation = input.uBoneLocation.xy;
+
+	// bone 0
+	boneLocation.y = input.blendIndex[0];
+	skinMatrix = input.blendWeight[0] * getTransformFromTexture(boneLocation);
+
+	// bone 1
+	boneLocation.y = input.blendIndex[1];
+	skinMatrix += input.blendWeight[1] * getTransformFromTexture(boneLocation);
+
+	// bone 2
+	boneLocation.y = input.blendIndex[2];
+	skinMatrix += input.blendWeight[2] * getTransformFromTexture(boneLocation);
+
+	// bone 3
+	boneLocation.y = input.blendIndex[3];
+	skinMatrix += input.blendWeight[3] * getTransformFromTexture(boneLocation);
+
+	// skin result
+	skinPosition = mul(input.pos, skinMatrix);
+
+	output.pos = mul(skinPosition, uMvpMatrix);
+	output.vPos = output.pos;
+
+	return output;
+}
