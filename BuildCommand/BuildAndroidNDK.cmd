01dddd38af4fef840e1e191eda02f3a6f5edfc1d
ducphamhong <hongduc.pr@gmail.com>
2024-05-15 12:01:53 +0700

    Update README.md and fix build

--- BuildCommand/BuildAndroidNDK.cmd
+++ BuildCommand/BuildAndroidNDK.cmd
@@ -1,13 +1,13 @@
-set MINGW=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
-set NDK=C:\SDK\android-ndk-r21e
-set MINSDKVERSION=24
+set MINGW=C:\MinGW\bin
+set NDK=C:\Android\android-ndk-r26d
+set SDK_VERSION=26
 cd..
 set PATH=%PATH%;%MINGW%
 
 set ABI="armeabi-v7a"
-cmake -S . -B ./PrjAndroid-%ABI% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%ABI% -DANDROID_ARM_NEON=ON -DANDROID_NATIVE_API_LEVEL=%MINSDKVERSION%
+cmake -S . -B ./PrjAndroid-%ABI% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%ABI% -DANDROID_ARM_NEON=ON -DANDROID_NATIVE_API_LEVEL=%SDK_VERSION%
 mingw32-make -C PrjAndroid-%ABI%
 
 set ABI="arm64-v8a"
-cmake -S . -B ./PrjAndroid-%ABI% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%ABI% -DANDROID_ARM_NEON=ON -DANDROID_NATIVE_API_LEVEL=%MINSDKVERSION%
+cmake -S . -B ./PrjAndroid-%ABI% -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake -DANDROID_ABI=%ABI% -DANDROID_ARM_NEON=ON -DANDROID_NATIVE_API_LEVEL=%SDK_VERSION%
 mingw32-make -C PrjAndroid-%ABI%
