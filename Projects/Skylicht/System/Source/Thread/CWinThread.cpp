15567b4806c8210a33e8adbc657ee81b167a1d42
Pham Hong Duc <hongduc.pr@gmail.com>
2024-06-10 08:43:35 +0700

    Update Windows system thread (test on GithubCI)

---
+++ Projects/Skylicht/System/Source/Thread/CWinThread.cpp
@@ -0,0 +1,123 @@
+/*
+!@
+MIT License
+
+Copyright (c) 2024 Skylicht Technology CO., LTD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This file is part of the "Skylicht Engine".
+https://github.com/skylicht-lab/skylicht-engine
+!#
+*/
+
+#include "stdafx.h"
+#include "CWinThread.h"
+
+#ifdef USE_WINTHREAD
+
+namespace SkylichtSystem
+{
+	DWORD WINAPI threadFunction(LPVOID lpParam)
+	{
+		CWinThread* p = reinterpret_cast<CWinThread*>(lpParam);
+		p->update();
+		return 0;
+	}
+
+	CWinThread::CWinThread(IThreadCallback* callback) :
+		IThread(callback),
+		m_run(false)
+	{
+		printf("[CWinThread] created\n");
+		DWORD threadID;
+		m_thread = CreateThread(
+			NULL,       // default security attributes
+			0,          // default stack size
+			(LPTHREAD_START_ROUTINE)threadFunction,
+			this,       // thread function arguments
+			0,          // default creation flags
+			&threadID); // receive thread identifier;
+
+		if (m_thread == NULL)
+		{
+			printf("[CWinThread] Warning: init error: %d\n", GetLastError());
+		}
+
+		m_loopMutex = CreateMutex(NULL, FALSE, NULL);
+	}
+
+	CWinThread::~CWinThread()
+	{
+		stop();
+
+		CloseHandle(m_thread);
+		CloseHandle(m_loopMutex);
+	}
+
+	void CWinThread::update()
+	{
+		if (m_callback == NULL)
+		{
+			printf("[CWinThread] quit - no Callback\n");
+			return;
+		}
+
+		printf("[CWinThread] run update\n");
+
+		// run thread
+		m_run = m_callback->enableThreadLoop();
+
+		bool needUnlock = false;
+		if (m_run)
+		{
+			WaitForSingleObject(m_loopMutex, INFINITE);
+			needUnlock = true;
+		}
+
+		m_callback->runThread();
+
+		// callback
+		while (m_run)
+		{
+			ReleaseMutex(m_loopMutex);
+			m_callback->updateThread();
+			WaitForSingleObject(m_loopMutex, INFINITE);
+		}
+
+		if (needUnlock)
+		{
+			ReleaseMutex(m_loopMutex);
+		}
+
+		printf("[CWinThread] end update\n");
+
+		// IThread::sleep(1);
+	}
+
+	void CWinThread::stop()
+	{
+		if (m_run)
+		{
+			WaitForSingleObject(m_loopMutex, INFINITE);
+			m_run = false;
+			ReleaseMutex(m_loopMutex);
+
+			WaitForSingleObject(m_thread, INFINITE);
+			printf("[CWinThread] stop!\n");
+		}
+	}
+}
+
+#endif
--- Projects/Skylicht/System/Source/Thread/CWinThread.h
+++ Projects/Skylicht/System/Source/Thread/CWinThread.h
@@ -2,7 +2,7 @@
 !@
 MIT License
 
-Copyright (c) 2019 Skylicht Technology CO., LTD
+Copyright (c) 2024 Skylicht Technology CO., LTD
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
@@ -24,19 +24,32 @@ https://github.com/skylicht-lab/skylicht-engine
 
 #pragma once
 
-#if defined(ANDROID)
-	#define USE_PTHREAD
-#elif defined(EMSCRIPTEN)
-	// no thread support on webgl
-#elif defined(IOS)
-	#define USE_PTHREAD
-#elif defined(MACOS)
-    #define USE_PTHREAD
-#elif defined(CYGWIN) || defined(MINGW)
-	#define USE_PTHREAD
-#elif defined(_WIN32)
-	#include <Windows.h>
-	#define USE_WINTHREAD
-#elif defined(LINUX)
-	#define USE_PTHREAD
+#include "IMutex.h"
+#include "IThread.h"
+
+#ifdef USE_WINTHREAD
+
+#include <Windows.h>
+
+namespace SkylichtSystem
+{
+	class CWinThread : public IThread
+	{
+	protected:
+		HANDLE m_thread;
+		HANDLE m_loopMutex;
+		bool m_run;
+
+	public:
+		CWinThread(IThreadCallback* callback);
+		virtual ~CWinThread();
+
+		virtual void update();
+
+		virtual void stop();
+
+		static void* run(void* param);
+	};
+}
+
 #endif
