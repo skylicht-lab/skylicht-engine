b00935944478756fb281fe2712c5ae2b944c8ffc
duc.phamhong <hongduc.pr@gmail.com>
2024-11-14 17:10:36 +0700

    Update build openssl & network in CMakeLists.txt

--- Samples/BoidSystemVAT/CMakeProject.cmake
+++ Samples/BoidSystemVAT/CMakeProject.cmake
@@ -1,12 +1,9 @@
 include_directories(
 	${SKYLICHT_ENGINE_SOURCE_DIR}/Samples/SampleFW
-	${SKYLICHT_ENGINE_SOURCE_DIR}/Samples/BoidSystem/Source/Boids
 )
 
 file(GLOB_RECURSE additional_source
 	../SampleFW/**.cpp
-	../SampleFW/**.h
-	../BoidSystem/Source/Boids/**.cpp
-	../BoidSystem/Source/Boids/**.h)
+	../SampleFW/**.h)
 	
 list (APPEND application_source ${additional_source})
