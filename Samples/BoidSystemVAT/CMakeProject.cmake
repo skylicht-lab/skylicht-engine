c0eba37f6916946d60d0736d951d7f1c178bc769
duc.phamhong <hongduc.pr@gmail.com>
2024-11-14 16:44:14 +0700

    Fix build error on prj BoidVAT

--- Samples/BoidSystemVAT/CMakeProject.cmake
+++ Samples/BoidSystemVAT/CMakeProject.cmake
@@ -1,9 +1,12 @@
 include_directories(
 	${SKYLICHT_ENGINE_SOURCE_DIR}/Samples/SampleFW
+	${SKYLICHT_ENGINE_SOURCE_DIR}/Samples/BoidSystem/Source/Boids
 )
 
 file(GLOB_RECURSE additional_source
 	../SampleFW/**.cpp
-	../SampleFW/**.h)
+	../SampleFW/**.h
+	../BoidSystem/Source/Boids/**.cpp
+	../BoidSystem/Source/Boids/**.h)
 	
 list (APPEND application_source ${additional_source})
