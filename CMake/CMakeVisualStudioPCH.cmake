005f5898e62d22638513c8369dff42d3603f5ab3
duc.pham <hongduc.pr@gmail.com>
2025-10-05 17:07:16 +0700

    Engine: Update for cmake ninja generator

--- CMake/CMakeVisualStudioPCH.cmake
+++ CMake/CMakeVisualStudioPCH.cmake
@@ -1,9 +1,11 @@
 function(target_precompiled_header project_target pch_source project_sources)
-if(MSVC)
+
+if(MSVC AND NOT "${CMAKE_GENERATOR}" STREQUAL "Ninja")
 	get_filename_component(pch_basename ${pch_source} NAME_WE)
 	set(pch_header "${pch_basename}.h")
 	
 	set_target_properties(${project_target} PROPERTIES COMPILE_FLAGS "/Yu${pch_header}")
 	set_source_files_properties(${pch_source} PROPERTIES COMPILE_FLAGS "/Yc${pch_header}")
-endif(MSVC)
+endif()
+
 endfunction()
